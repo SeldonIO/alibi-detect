888c38ca19f2809bab924b4a23e783f2a0537e2f
giovac73 <giova.physics@gmail.com>
2019-10-22 16:43:09 +0100

    rolling stats updated with new vae methods

--- odcd/utils/streaming_sim_test.py
+++ odcd/utils/streaming_sim_test.py
@@ -56,6 +56,112 @@ def get_trust_score(x, clf, enc, tsc):
     return score
 
 
+def rolling_stats(vae, X_orig, y_orig, X_cd, y_cd, cd_start, cd_full, nb_samples_tot, start, amp=1,
+                  clf=None, tsc=None, enc=None):
+
+    m, m_ts = Mean(), Mean()
+    a = Mean()
+    v, v_ts = Var(), Var()
+
+    rws = []
+    entrs = []
+    scores = []
+    probs = []
+
+    acc = []
+
+    mu = []
+    var = []
+    mu_ts = []
+    var_ts = []
+
+    ps = []
+    y_orig = np.argmax(y_orig, axis=1)
+    y_cd = np.argmax(y_cd, axis=1)
+    for i in range(nb_samples_tot):
+        if i % 1000 == 0:
+            print('Sample {} of {}'.format(i, nb_samples_tot))
+
+        if i < cd_start:
+            p = 0
+        elif i >= cd_start and i < cd_full:
+            p = (i - cd_start) / (cd_full - cd_start)
+        else:
+            p = 1
+
+        pp = np.random.choice([0, 1], p=[p, 1 - p])
+        if pp == 1:
+            idx = np.random.choice(range(len(X_orig)))
+            x = X_orig[idx].reshape((1,) + X_orig.shape[1:])
+            y = y_orig[idx]
+        else:
+            idx = np.random.choice(range(len(X_cd)))
+            x = X_cd[idx].reshape((1, ) + X_cd.shape[1:])
+            y = y_cd[idx]
+
+        #vae_outs_test = vae.vae.predict(x)
+        #symm_samples_test = vae_outs_test[0]
+        #orig_preds_test = vae_outs_test[1]
+        #trans_preds_test = vae_outs_test[2]
+        # kl_test = entropy(orig_preds_test.T, trans_preds_test.T)[0]
+
+        orig_preds_test = vae.predict_original(x)
+        kl_test = vae.signal(x, amp=amp)
+        pred = np.argmax(orig_preds_test, axis=1)
+        pred_prob = orig_preds_test[:, pred[0]]
+        r = is_good(pred, y)
+
+        a.update(r)
+        m.update(kl_test)
+        v.update(kl_test)
+        mm, vv, aa = m.get(), v.get(), a.get()
+
+        mu.append(mm)
+        var.append(vv)
+        if clf is not None and enc is not None and tsc is not None:
+            score = get_trust_score(x, clf, enc, tsc)
+            m_ts.update(score)
+            v_ts.update(score)
+            mm_ts, vv_ts = m_ts.get(), v_ts.get()
+            mm_ts = mm_ts[0]
+            scores.append(score)
+            mu_ts.append(mm_ts)
+            var_ts.append(vv_ts)
+
+        acc.append(aa)
+        ps.append(p)
+
+        rws.append(r)
+        entrs.append(kl_test)
+        probs.append(pred_prob)
+
+    cd_start = cd_start - start
+    cd_full = cd_full - start
+    df = pd.DataFrame()
+    df['rw'] = rws[start:]
+    # df['rw'] = df['rw'].apply(lambda x:'r' if x == 0 else 'b')
+    df['entropy'] = entrs[start:]
+    if clf is not None and enc is not None and tsc is not None:
+        df['ts'] = scores[start:]
+    df['pred_prob'] = probs[start:]
+    df['accuracy'] = acc[start:]
+    df['contamination'] = ps[start:]
+    df['entropy_mean'] = mu[start:]
+    df['entropy_var'] = var[start:]
+    if clf is not None and enc is not None and tsc is not None:
+        df['ts_mean'] = mu_ts[start:]
+        df['ts_var'] = var_ts[start:]
+
+    return df
+
+
+def is_good(y_pred, y_true):
+    if y_pred == y_true:
+        return 1
+    else:
+        return 0
+
+
 def sliding_window(vae, X_orig, y_orig,  X_cd, y_cd, window_size, cd_start, nb_samples_tot, start,
                    clf=None, tsc=None, enc=None):
 
@@ -147,107 +253,3 @@ def sliding_window(vae, X_orig, y_orig,  X_cd, y_cd, window_size, cd_start, nb_s
         df['ts_var'] = var_ts[start:]
 
     return df, window_start, window_full
-
-
-def rolling_stats(vae, X_orig, y_orig, X_cd, y_cd, cd_start, cd_full, nb_samples_tot, start,
-                  clf=None, tsc=None, enc=None):
-
-    m, m_ts = Mean(), Mean()
-    a = Mean()
-    v, v_ts = Var(), Var()
-
-    rws = []
-    entrs = []
-    scores = []
-    probs = []
-
-    acc = []
-
-    mu = []
-    var = []
-    mu_ts = []
-    var_ts = []
-
-    ps = []
-    y_orig = np.argmax(y_orig, axis=1)
-    y_cd = np.argmax(y_cd, axis=1)
-    for i in range(nb_samples_tot):
-        if i % 1000 == 0:
-            print('Sample {} of {}'.format(i, nb_samples_tot))
-
-        if i < cd_start:
-            p = 0
-        elif i >= cd_start and i < cd_full:
-            p = (i - cd_start) / (cd_full - cd_start)
-        else:
-            p = 1
-
-        pp = np.random.choice([0, 1], p=[p, 1 - p])
-        if pp == 1:
-            idx = np.random.choice(range(len(X_orig)))
-            x = X_orig[idx].reshape((1,) + X_orig.shape[1:])
-            y = y_orig[idx]
-        else:
-            idx = np.random.choice(range(len(X_cd)))
-            x = X_cd[idx].reshape((1, ) + X_cd.shape[1:])
-            y = y_cd[idx]
-
-        vae_outs_test = vae.vae.predict(x)
-        symm_samples_test = vae_outs_test[0]
-        orig_preds_test = vae_outs_test[1]
-        trans_preds_test = vae_outs_test[2]
-
-        kl_test = entropy(orig_preds_test.T, trans_preds_test.T)[0]
-        pred = np.argmax(orig_preds_test, axis=1)
-        pred_prob = orig_preds_test[:, pred[0]]
-        r = is_good(pred, y)
-
-        a.update(r)
-        m.update(kl_test)
-        v.update(kl_test)
-        mm, vv, aa = m.get(), v.get(), a.get()
-
-        mu.append(mm)
-        var.append(vv)
-        if clf is not None and enc is not None and tsc is not None:
-            score = get_trust_score(x, clf, enc, tsc)
-            m_ts.update(score)
-            v_ts.update(score)
-            mm_ts, vv_ts = m_ts.get(), v_ts.get()
-            mm_ts = mm_ts[0]
-            scores.append(score)
-            mu_ts.append(mm_ts)
-            var_ts.append(vv_ts)
-
-        acc.append(aa)
-        ps.append(p)
-
-        rws.append(r)
-        entrs.append(kl_test)
-        probs.append(pred_prob)
-
-    cd_start = cd_start - start
-    cd_full = cd_full - start
-    df = pd.DataFrame()
-    df['rw'] = rws[start:]
-    # df['rw'] = df['rw'].apply(lambda x:'r' if x == 0 else 'b')
-    df['entropy'] = entrs[start:]
-    if clf is not None and enc is not None and tsc is not None:
-        df['ts'] = scores[start:]
-    df['pred_prob'] = probs[start:]
-    df['accuracy'] = acc[start:]
-    df['contamination'] = ps[start:]
-    df['entropy_mean'] = mu[start:]
-    df['entropy_var'] = var[start:]
-    if clf is not None and enc is not None and tsc is not None:
-        df['ts_mean'] = mu_ts[start:]
-        df['ts_var'] = var_ts[start:]
-
-    return df
-
-
-def is_good(y_pred, y_true):
-    if y_pred == y_true:
-        return 1
-    else:
-        return 0
