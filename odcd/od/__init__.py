0472a346477436a386974488befe44cb887dd5ac
Arnaud Van Looveren <avl@seldon.io>
2019-11-06 11:02:22 +0000

    update init files

--- odcd/od/__init__.py
+++ odcd/od/__init__.py
@@ -1,1 +1,7 @@
+from .aegmm import OutlierAEGMM
+from .isolationforest import IForest
+from .mahalanobis import Mahalanobis
+from .vae import OutlierVAE
+from .vaegmm import OutlierVAEGMM
 
+__all__ = ["OutlierAEGMM", "IForest", "Mahalanobis", "OutlierVAE", "OutlierVAEGMM"]
