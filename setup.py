7b6c77e9e10cb539193b0d33e23e96dbd521ba13
Arnaud Van Looveren <avl@seldon.io>
2020-02-17 11:54:30 +0000

    fix prophet

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         "creme",
         "fbprophet",
-        "holidays>=0.9.11",
+        "holidays==0.9.11",
         "matplotlib",
         "numpy",
         "pandas",
