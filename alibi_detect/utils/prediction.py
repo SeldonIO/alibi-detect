e8576f73650a956245fd2005a9a34b79f336548e
Arnaud Van Looveren <avl@seldon.io>
2020-02-26 17:15:13 +0000

    remove unused import

--- alibi_detect/utils/prediction.py
+++ alibi_detect/utils/prediction.py
@@ -1,7 +1,7 @@
 import logging
 import numpy as np
 import tensorflow as tf
-from typing import Callable, Tuple, Union
+from typing import Callable, Union
 from alibi_detect.models.autoencoder import AE, AEGMM, Seq2Seq, VAE, VAEGMM
 
 logger = logging.getLogger(__name__)
