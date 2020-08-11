44f19e70062179b4543fb12b4fceefc4541f9bb4
Janis Klaise <jk@seldon.io>
2020-08-11 11:02:03 +0100

    Add Python 3.8 support (#125)

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     license="Apache 2.0",
     packages=find_packages(),
     include_package_data=True,
-    python_requires=">3.5.1",
+    python_requires=">=3.6",
     setup_requires=["pytest-runner"],
     install_requires=[
         "creme",
