16013797f09097297aed6d4b1b2732a701421cd8
Arnaud Van Looveren <avl@seldon.io>
2020-09-09 13:04:41 +0100

    v0.4.2

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         "creme",
         "dask[array]",
-        "fbprophet",
+        "fbprophet>=0.5,<0.7",
         "holidays==0.9.11",
         "matplotlib",
         "numpy",
