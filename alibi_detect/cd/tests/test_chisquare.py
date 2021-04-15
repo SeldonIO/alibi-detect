22af47f9c23d7c43f56e473b8b9e29e5baae4ad9
Arnaud Van Looveren <avl@seldon.io>
2021-04-15 19:26:14 +0100

    update tests

--- alibi_detect/cd/tests/test_chisquare.py
+++ alibi_detect/cd/tests/test_chisquare.py
@@ -7,11 +7,18 @@ n_categories, n_features, n_tiles = 5, 6, 20
 x_ref = np.tile(np.array([np.arange(n_categories)] * n_features).T, (n_tiles, 1))
 np.random.shuffle(x_ref)
 
+categories_per_feature = [
+    None,
+    {f: n_categories for f in range(n_features)},
+    {f: list(np.arange(n_categories)) for f in range(n_features)}
+]
 preprocess = [None]
 correction = ['bonferroni', 'fdr']
 update_x_ref = [{'last': 1000}, {'reservoir_sampling': 1000}]
 preprocess_x_ref = [True, False]
-tests_chisquaredrift = list(product(preprocess, correction, update_x_ref, preprocess_x_ref))
+new_categories = [True, False]
+tests_chisquaredrift = list(product(categories_per_feature, preprocess, correction,
+                                    update_x_ref, preprocess_x_ref, new_categories))
 n_tests = len(tests_chisquaredrift)
 
 
@@ -22,19 +29,26 @@ def chisquaredrift_params(request):
 
 @pytest.mark.parametrize('chisquaredrift_params', list(range(n_tests)), indirect=True)
 def test_chisquaredrift(chisquaredrift_params):
-    preprocess_fn, correction, update_x_ref, preprocess_x_ref = chisquaredrift_params
+    categories_per_feature, preprocess_fn, correction, \
+        update_x_ref, preprocess_x_ref, new_categories = chisquaredrift_params
 
     cd = ChiSquareDrift(
         x_ref=x_ref,
         p_val=.05,
+        categories_per_feature=categories_per_feature,
         preprocess_x_ref=preprocess_x_ref,
         update_x_ref=update_x_ref,
         preprocess_fn=preprocess_fn,
         correction=correction,
     )
     x = x_ref.copy()
+    if new_categories:
+        x = x + 1
     preds_batch = cd.predict(x, drift_type='batch', return_p_val=True)
-    assert preds_batch['data']['is_drift'] == 0
+    if new_categories:
+        assert preds_batch['data']['is_drift'] == 1
+    else:
+        assert preds_batch['data']['is_drift'] == 0
     k = list(update_x_ref.keys())[0]
     assert cd.n == x.shape[0] + x_ref.shape[0]
     assert cd.x_ref.shape[0] == min(update_x_ref[k], x.shape[0] + x_ref.shape[0])
