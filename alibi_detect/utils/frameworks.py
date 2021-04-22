2b040022917e73ecbeee2ae7593150d4e5aea8b0
Aman Prakash Jha <amanjha7151@gmail.com>
2021-04-23 00:22:11 +0530

    Factor out backend issue 230

--- alibi_detect/utils/frameworks.py
+++ alibi_detect/utils/frameworks.py
@@ -9,3 +9,12 @@ try:
     has_pytorch = True
 except ImportError:
     has_pytorch = False
+
+def validate_backend(backend):
+    BACKENDS = ['tensorflow', 'pytorch']
+    backend = backend.lower() 
+    if backend == 'tensorflow' and not has_tensorflow or backend == 'pytorch' and not has_pytorch: 
+        raise ImportError(f'{backend} not installed. Cannot initialize and run the ' 
+                        f'ClassifierDrift detector with {backend} backend.') 
+    elif backend not in BACKENDS: 
+        raise NotImplementedError(f'{backend} not implemented. Use tensorflow or pytorch instead.') 
