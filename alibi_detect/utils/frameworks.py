f3bfd34495eef261bca2d99c593c151906f701d3
Aman Prakash Jha <amanjha7151@gmail.com>
2021-04-23 01:34:31 +0530

    Flake8 rules followed

--- alibi_detect/utils/frameworks.py
+++ alibi_detect/utils/frameworks.py
@@ -10,11 +10,12 @@ try:
 except ImportError:
     has_pytorch = False
 
+
 def validate_backend(backend):
     BACKENDS = ['tensorflow', 'pytorch']
-    backend = backend.lower() 
-    if backend == 'tensorflow' and not has_tensorflow or backend == 'pytorch' and not has_pytorch: 
-        raise ImportError(f'{backend} not installed. Cannot initialize and run the ' 
-                        f'ClassifierDrift detector with {backend} backend.') 
-    elif backend not in BACKENDS: 
-        raise NotImplementedError(f'{backend} not implemented. Use tensorflow or pytorch instead.') 
+    backend = backend.lower()
+    if backend == 'tensorflow' and not has_tensorflow or backend == 'pytorch' and not has_pytorch:
+        raise ImportError(f'{backend} not installed. Cannot initialize and run the '
+                          f'ClassifierDrift detector with {backend} backend.')
+    elif backend not in BACKENDS:
+        raise NotImplementedError(f'{backend} not implemented. Use tensorflow or pytorch instead.')
