2815bbf7c6204b67ccafe43527a87caaae8ae111
Arnaud Van Looveren <avl@seldon.io>
2021-06-28 13:04:34 +0100

    update lsdd test

--- alibi_detect/cd/pytorch/tests/test_lsdd_pt.py
+++ alibi_detect/cd/pytorch/tests/test_lsdd_pt.py
@@ -4,7 +4,7 @@ import numpy as np
 import pytest
 import torch
 import torch.nn as nn
-from typing import Callable
+from typing import Callable, List
 from alibi_detect.cd.pytorch.lsdd import LSDDDriftTorch
 from alibi_detect.cd.pytorch.preprocess import HiddenOutput, preprocess_drift
 
@@ -22,11 +22,16 @@ class MyModel(nn.Module):
         return self.dense2(x)
 
 
+def preprocess_list(x: List[np.ndarray]) -> np.ndarray:
+    return np.concatenate(x, axis=0)
+
+
 n_features = [10]
 n_enc = [None, 3]
 preprocess = [
     (None, None),
-    (preprocess_drift, {'model': HiddenOutput, 'layer': -1})
+    (preprocess_drift, {'model': HiddenOutput, 'layer': -1}),
+    (preprocess_list, None)
 ]
 update_x_ref = [None]
 preprocess_x_ref = [True, False]
@@ -50,7 +55,13 @@ def test_lsdd(lsdd_params):
 
     x_ref = np.random.randn(n * n_features).reshape(n, n_features).astype(np.float32)
     preprocess_fn, preprocess_kwargs = preprocess
-    if isinstance(preprocess_fn, Callable) and 'layer' in list(preprocess_kwargs.keys()) \
+    to_list = False
+    if hasattr(preprocess_fn, '__name__') and preprocess_fn.__name__ == 'preprocess_list':
+        if not preprocess_x_ref:
+            return
+        to_list = True
+        x_ref = [_[None, :] for _ in x_ref]
+    elif isinstance(preprocess_fn, Callable) and 'layer' in list(preprocess_kwargs.keys()) \
             and preprocess_kwargs['model'].__name__ == 'HiddenOutput':
         model = MyModel(n_features)
         layer = preprocess_kwargs['layer']
@@ -73,10 +84,12 @@ def test_lsdd(lsdd_params):
     assert preds['data']['is_drift'] == 0 and preds['data']['p_val'] >= cd.p_val
     if isinstance(update_x_ref, dict):
         k = list(update_x_ref.keys())[0]
-        assert cd.n == x.shape[0] + x_ref.shape[0]
-        assert cd.x_ref.shape[0] == min(update_x_ref[k], x.shape[0] + x_ref.shape[0])
+        assert cd.n == len(x) + len(x_ref)
+        assert cd.x_ref.shape[0] == min(update_x_ref[k], len(x) + len(x_ref))
 
     x_h1 = np.random.randn(n * n_features).reshape(n, n_features).astype(np.float32)
+    if to_list:
+        x_h1 = [_[None, :] for _ in x_h1]
     preds = cd.predict(x_h1, return_p_val=True)
     if preds['data']['is_drift'] == 1:
         assert preds['data']['p_val'] < preds['data']['threshold'] == cd.p_val
