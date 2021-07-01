208002e56580fedafb547dc9b6a5205844adeb12
Oliver Cobb <55446819+ojcobb@users.noreply.github.com>
2021-07-01 11:09:20 +0100

    Remove pca function and associated tests

--- alibi_detect/cd/preprocess.py
+++ alibi_detect/cd/preprocess.py
@@ -1,33 +1,7 @@
 import numpy as np
-from sklearn.decomposition import PCA
 from scipy.special import softmax
 from scipy.stats import entropy
 from typing import Callable
-
-
-def pca(X: np.ndarray, n_components: int = 2, svd_solver: str = 'auto') -> np.ndarray:
-    """
-    Apply PCA dimensionality reduction and return the projection of X on
-    the first `n_components` principal components.
-
-    Parameters
-    ----------
-    X
-        Batch of instances.
-    n_components
-        Number of principal component projections to return.
-    svd_solver
-        Solver used for SVD. Options are ‘auto’, ‘full’, ‘arpack’ or ‘randomized’.
-
-    Returns
-    -------
-    Projection of X on first `n_components` principcal components.
-    """
-    X = X.reshape(X.shape[0], -1)
-    pca = PCA(n_components=n_components, svd_solver=svd_solver)
-    pca.fit(X)
-    X_pca = pca.transform(X)
-    return X_pca
 
 
 def classifier_uncertainty(
