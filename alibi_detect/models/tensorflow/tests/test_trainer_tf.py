a3631e7fec02f2fa34d61d2bfb9964e69c0213eb
Arnaud Van Looveren <avl@seldon.io>
2021-07-20 13:18:59 +0100

    update tf trainer test

--- alibi_detect/models/tensorflow/tests/test_trainer_tf.py
+++ alibi_detect/models/tensorflow/tests/test_trainer_tf.py
@@ -16,29 +16,35 @@ outputs = tf.keras.layers.Dense(F, activation=tf.nn.softmax)(inputs)
 model = tf.keras.Model(inputs=inputs, outputs=outputs)
 check_model_weights = model.weights[0].numpy()
 
+
+def preprocess_fn(x: np.ndarray) -> np.ndarray:
+    return x
+
+
 X_train = [x]
 y_train = [None, y]
 dataset = [partial(TFDataset, batch_size=10), None]
 loss_fn_kwargs = [None, {'from_logits': False}]
+preprocess = [preprocess_fn, None]
 verbose = [False, True]
 
-tests = list(product(X_train, y_train, dataset, loss_fn_kwargs, verbose))
+tests = list(product(X_train, y_train, dataset, loss_fn_kwargs, preprocess, verbose))
 n_tests = len(tests)
 
 
 @pytest.fixture
 def trainer_params(request):
-    x_train, y_train, dataset, loss_fn_kwargs, verbose = tests[request.param]
-    return x_train, y_train, dataset, loss_fn_kwargs, verbose
+    x_train, y_train, dataset, loss_fn_kwargs, preprocess, verbose = tests[request.param]
+    return x_train, y_train, dataset, loss_fn_kwargs, preprocess, verbose
 
 
 @pytest.mark.parametrize('trainer_params', list(range(n_tests)), indirect=True)
 def test_trainer(trainer_params):
-    x_train, y_train, dataset, loss_fn_kwargs, verbose = trainer_params
+    x_train, y_train, dataset, loss_fn_kwargs, preprocess, verbose = trainer_params
     if dataset is not None and y_train is not None:
         ds = dataset(x_train, y_train)
     else:
         ds = None
     trainer(model, categorical_crossentropy, x_train, y_train=y_train, dataset=ds,
-            loss_fn_kwargs=loss_fn_kwargs, epochs=2, verbose=verbose)
+            loss_fn_kwargs=loss_fn_kwargs, preprocess_fn=preprocess, epochs=2, verbose=verbose)
     assert (model.weights[0].numpy() != check_model_weights).any()
