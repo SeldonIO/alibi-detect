85d5a7d0015a6138075458213e92794aa1950fbc
Ashley Scillitoe <ashley.scillitoe@seldon.io>
2021-12-06 13:10:22 +0000

    nbsphinx_link removed

--- testing/test_notebooks.py
+++ testing/test_notebooks.py
@@ -4,6 +4,7 @@ plugin. This approach may be more flexible if our requirements change in the fut
 """
 
 import glob
+import os
 from pathlib import Path
 import pytest
 from jupytext.cli import jupytext
@@ -14,8 +15,8 @@ from jupytext.cli import jupytext
 # changed notebooks. `pytest` does not allow `/` as part of the test name for the -k argument.
 # This also means that the approach is limited to all notebooks being in the `NOTEBOOK_DIR`
 # top-level path.
-NOTEBOOK_DIR = 'examples'
-ALL_NOTEBOOKS = {Path(x).name for x in glob.glob('examples/*.ipynb')}
+NOTEBOOK_DIR = 'doc/source/examples'
+ALL_NOTEBOOKS = {Path(x).name for x in glob.glob(os.path.join(NOTEBOOK_DIR, '*.ipynb'))}
 
 # The following set includes notebooks which are not to be executed during notebook tests.
 # These are typically those that would take too long to run in a CI environment or impractical
