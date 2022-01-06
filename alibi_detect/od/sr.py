e29cd7b6b46d2a3f97b02e63618f48f165b56bfa
RobertSamoilescu <robert.samoilescu@gmail.com>
2022-01-06 10:29:50 -0600

    fixed SR begining/ending spikes (#396)
    * resolved conflicts
    
    * moved constants into enumerate. included  types
    
    * included padding tests
    
    * fixed Literal typing
    
    * included _types
    
    * commented data.meta['attack_type'] == attack
    
    * refactored the test
    
    * updated docs and error handling
    
    * included new kwargs
    
    * add example
    
    * fixed minor docs warning
    
    * add new kwargs in doc/source/od/methods/sr.ipynb

--- alibi_detect/od/sr.py
+++ alibi_detect/od/sr.py
@@ -1,11 +1,26 @@
+import enum
 import logging
 import numpy as np
 from typing import Dict
 from alibi_detect.base import BaseDetector, ThresholdMixin, outlier_prediction_dict
+from alibi_detect.utils._types import Literal
 
 logger = logging.getLogger(__name__)
 
+# numerical stability for division
 EPSILON = 1e-8
+
+
+class Padding(str, enum.Enum):
+    CONSTANT = 'constant'
+    REPLICATE = 'replicate'
+    REFLECT = 'reflect'
+
+
+class Side(str, enum.Enum):
+    BILATERAL = 'bilateral'
+    LEFT = 'left'
+    RIGHT = 'right'
 
 
 class SpectralResidual(BaseDetector, ThresholdMixin):
@@ -14,6 +29,9 @@ class SpectralResidual(BaseDetector, ThresholdMixin):
                  threshold: float = None,
                  window_amp: int = None,
                  window_local: int = None,
+                 padding_amp_method: Literal['constant', 'replicate', 'reflect'] = 'reflect',
+                 padding_local_method: Literal['constant', 'replicate', 'reflect'] = 'reflect',
+                 padding_amp_side: Literal['bilateral', 'left', 'right'] = 'bilateral',
                  n_est_points: int = None,
                  n_grad_points: int = 5,
                  ) -> None:
@@ -29,7 +47,32 @@ class SpectralResidual(BaseDetector, ThresholdMixin):
         window_amp
             Window for the average log amplitude.
         window_local
-            Window for the local average of the saliency map.
+            Window for the local average of the saliency map. Note that the averaging is performed over the
+            previous `window_local` data points (i.e., is a local average of the preceding `window_local` points for
+            the current index).
+        padding_amp_method
+            Padding method to be used prior to each convolution over log amplitude.
+            Possible values: `constant` | `replicate` | `reflect`. Default value: `replicate`.
+
+             - `constant` - padding with constant 0.
+
+             - `replicate` - repeats the last/extreme value.
+
+             - `reflect` - reflects the time series.
+
+        padding_local_method
+            Padding method to be used prior to each convolution over saliency map.
+            Possible values: `constant` | `replicate` | `reflect`. Default value: `replicate`.
+
+             - `constant` - padding with constant 0.
+
+             - `replicate` - repeats the last/extreme value.
+
+             - `reflect` - reflects the time series.
+
+        padding_amp_side
+            Whether to pad the amplitudes on both sides or only on one side.
+            Possible values: `bilateral` | `left` | `right`.
         n_est_points
             Number of estimated points padded to the end of the sequence.
         n_grad_points
@@ -39,15 +82,27 @@ class SpectralResidual(BaseDetector, ThresholdMixin):
         super().__init__()
 
         if threshold is None:
-            logger.warning('No threshold level set. Need to infer threshold using `infer_threshold`.')
+            logger.warning("No threshold level set. Need to infer threshold using `infer_threshold`.")
 
         self.threshold = threshold
         self.window_amp = window_amp
         self.window_local = window_local
-        self.conv_amp = np.ones((1, window_amp)).reshape(-1, ) / window_amp
-        self.conv_local = np.ones((1, window_local)).reshape(-1, ) / window_local
+        self.conv_amp = np.ones((1, window_amp)).reshape(-1,) / window_amp
+
+        # conv_local needs a special treatment since the paper says that:
+        # \bar{S}(xi) is the local average of the preceding z points of S(xi).
+        # To use the same padding implementation that includes the current point we convolving, we define a modified
+        # filter given by: [0, 1, 1, 1,  ... ,1] / window_local of size `window_local + 1`. In this way
+        # the current value is multiplied by 0 and thus neglected. Note that the 0 goes first since before the
+        # element-wise multiplication, the filter is flipped. We only do this since the filter is asymmetric.
+        self.conv_local = np.ones((1, window_local + 1)).reshape(-1,) / window_local
+        self.conv_local[0] = 0
+
         self.n_est_points = n_est_points
         self.n_grad_points = n_grad_points
+        self.padding_amp_method = padding_amp_method
+        self.padding_local_method = padding_local_method
+        self.padding_amp_side = padding_amp_side
 
         # set metadata
         self.meta['detector_type'] = 'online'
@@ -65,9 +120,13 @@ class SpectralResidual(BaseDetector, ThresholdMixin):
         Parameters
         ----------
         X
-            Batch of instances.
+            Uniformly sampled time series instances.
+        t
+            Equidistant timestamps corresponding to each input instances (i.e, the array should contain
+            numerical values in increasing order). If not provided, the timestamps will be replaced by an array of
+            integers `[0, 1, ... , N - 1]`, where `N` is the size of the input time series.
         threshold_perc
-            Percentage of X considered to be normal based on the outlier score.
+            Percentage of `X` considered to be normal based on the outlier score.
         """
         if t is None:
             t = np.arange(X.shape[0])
@@ -78,6 +137,96 @@ class SpectralResidual(BaseDetector, ThresholdMixin):
         # update threshold
         self.threshold = np.percentile(iscore, threshold_perc)
 
+    @staticmethod
+    def pad_same(X: np.ndarray,
+                 W: np.ndarray,
+                 method: str = 'replicate',
+                 side: str = 'bilateral') -> np.ndarray:
+        """
+        Adds padding to the time series `X` such that after applying a valid convolution with a kernel/filter
+        `w`, the resulting time series has the same shape as the input `X`.
+
+        Parameters
+        ----------
+        X
+            Time series to be padded
+        W
+            Convolution kernel/filter.
+        method
+            Padding method to be used.
+            Possible values:
+
+             - `constant` - padding with constant 0.
+
+             - `replicate` - repeats the last/extreme value.
+
+             - `reflect` - reflects the time series.
+        side
+            Whether to pad the time series bilateral or only on one side.
+            Possible values:
+
+             - `bilateral` - time series is padded on both sides.
+
+             - `left` - time series is padded only on the left hand side.
+
+             - `right` - time series is padded only on the right hand side.
+
+        Returns
+        -------
+        Padded time series.
+        """
+        paddings = [p.value for p in Padding]
+        if method not in paddings:
+            raise ValueError(f"Unknown padding method. Received '{method}'. Select one of the following: {paddings}.")
+
+        sides = [s.value for s in Side]
+        if side not in sides:
+            raise ValueError(f"Unknown padding side. Received '{side}'. Select one of the following: {sides}.")
+
+        if len(X.shape) != 1:
+            raise ValueError(f"Only 1D time series supported. Received a times series with {len(X.shape)} dimensions.")
+
+        if len(W.shape) != 1:
+            raise ValueError("Only 1D kernel/filter supported. Received a kernel/filter "
+                             f"with {len(W.shape)} dimensions.")
+
+        pad_size = W.shape[0] - 1
+
+        if side == Side.BILATERAL:
+            pad_size_right = pad_size // 2
+            pad_size_left = pad_size - pad_size_right
+
+        elif side == Side.LEFT:
+            pad_size_right = 0
+            pad_size_left = pad_size
+
+        else:
+            pad_size_right = pad_size
+            pad_size_left = 0
+
+        # replicate padding
+        if method == Padding.REPLICATE:
+            return np.concatenate([
+                np.tile(X[0], pad_size_left),
+                X,
+                np.tile(X[-1], pad_size_right)
+            ])
+
+        # reflection padding
+        if method == Padding.REFLECT:
+            return np.concatenate([
+                X[1:pad_size_left + 1][::-1],
+                X,
+                X[-pad_size_right - 1: -1][::-1] if pad_size_right > 0 else np.array([])
+            ])
+
+        # zero padding
+        return np.concatenate([
+            np.tile(0, pad_size_left),
+            X,
+            np.tile(0, pad_size_right)
+        ])
+
     def saliency_map(self, X: np.ndarray) -> np.ndarray:
         """
         Compute saliency map.
@@ -85,18 +234,40 @@ class SpectralResidual(BaseDetector, ThresholdMixin):
         Parameters
         ----------
         X
-            Time series of instances.
+            Uniformly sampled time series instances.
 
         Returns
         -------
         Array with saliency map values.
         """
+        if X.shape[0] <= self.window_amp:
+            raise ValueError("The length of the input time series should be greater than the amplitude window. "
+                             f"Received an input times series of length {X.shape[0]} and an amplitude "
+                             f"window of {self.window_amp}.")
+
         fft = np.fft.fft(X)
         amp = np.abs(fft)
         log_amp = np.log(amp)
         phase = np.angle(fft)
-        ma_log_amp = np.convolve(log_amp, self.conv_amp, 'same')
-        res_amp = log_amp - ma_log_amp  # type: np.ndarray
+        # split spectrum into bias term and symmetric frequencies
+        bias, sym_freq = log_amp[:1], log_amp[1:]
+        # select just the first half of the sym_freq
+        freq = sym_freq[:(len(sym_freq) + 1) // 2]
+        # apply filter/moving average, but first pad the `freq` array
+        padded_freq = SpectralResidual.pad_same(X=freq,
+                                                W=self.conv_amp,
+                                                method=self.padding_amp_method,
+                                                side=self.padding_amp_side)
+        ma_freq = np.convolve(padded_freq, self.conv_amp, 'valid')
+        # construct moving average log amplitude spectrum
+        ma_log_amp = np.concatenate([
+            bias,
+            ma_freq,
+            (ma_freq[:-1] if len(sym_freq) % 2 == 1 else ma_freq)[::-1]
+        ])
+        assert ma_log_amp.shape[0] == log_amp.shape[0], "`ma_log_amp` size does not match `log_amp` size."
+        # compute residual spectrum and transform back to time domain
+        res_amp = log_amp - ma_log_amp
         sr = np.abs(np.fft.ifft(np.exp(res_amp + 1j * phase)))
         return sr
 
@@ -108,9 +279,10 @@ class SpectralResidual(BaseDetector, ThresholdMixin):
         Parameters
         ----------
         X
-            Time series of instances.
+            Uniformly sampled time series instances.
         t
-            Time steps.
+            Equidistant timestamps corresponding to each input instances (i.e, the array should contain
+            numerical values in increasing order).
 
         Returns
         -------
@@ -129,9 +301,10 @@ class SpectralResidual(BaseDetector, ThresholdMixin):
         Parameters
         ----------
         X
-            Time series of instances.
+            Uniformly sampled time series instances.
         t
-            Time steps.
+            Equidistant timestamps corresponding to each input instances (i.e, the array should contain
+            numerical values in increasing order).
 
         Returns
         -------
@@ -149,9 +322,11 @@ class SpectralResidual(BaseDetector, ThresholdMixin):
         Parameters
         ----------
         X
-            Time series of instances.
+            Uniformly sampled time series instances.
         t
-            Time steps.
+            Equidistant timestamps corresponding to each input instances (i.e, the array should contain
+            numerical values in increasing order). If not provided, the timestamps will be replaced by an array of
+            integers `[0, 1, ... , N - 1]`, where `N` is the size of the input time series.
 
         Returns
         -------
@@ -164,13 +339,29 @@ class SpectralResidual(BaseDetector, ThresholdMixin):
             n_samples, n_dim = X.shape
             X = X.reshape(-1, )
             if X.shape[0] != n_samples:
-                raise ValueError('Only univariate time series allowed for SR method. Number of features '
-                                 'of time series equals {}.'.format(n_dim))
+                raise ValueError("Only uni-variate time series allowed for SR method. Received a time "
+                                 f"series with {n_dim} features.")
 
         X_pad = self.add_est_points(X, t)  # add padding
-        sr = self.saliency_map(X_pad)  # compute saliency map
-        sr = sr[:-self.n_est_points]  # remove padding again
-        ma_sr = np.convolve(sr, self.conv_local, 'same')
+        sr = self.saliency_map(X_pad)      # compute saliency map
+        sr = sr[:-self.n_est_points]       # remove padding again
+
+        if X.shape[0] <= self.window_local:
+            raise ValueError("The length of the time series should be greater than the local window. "
+                             f"Received an input time series of length {X.shape[0]} and a local "
+                             f"window of {self.window_local}.")
+
+        # pad the spectral residual before convolving. By applying `replicate` or `reflect` padding we can
+        # remove some of the bias/outliers introduced at the beginning of the saliency map by a naive zero padding
+        # performed by numpy. The reason for left padding is explained in a comment in the constructor.
+        padded_sr = SpectralResidual.pad_same(X=sr,
+                                              W=self.conv_local,
+                                              method=self.padding_local_method,
+                                              side=Side.LEFT)
+        ma_sr = np.convolve(padded_sr, self.conv_local, 'valid')
+        assert sr.shape[0] == ma_sr.shape[0], "`ma_sr` size does not match `sr` size."
+
+        # compute the outlier score
         iscore = (sr - ma_sr) / (ma_sr + EPSILON)
         return iscore
 
@@ -185,23 +376,25 @@ class SpectralResidual(BaseDetector, ThresholdMixin):
         Parameters
         ----------
         X
-            Time series of instances.
+            Uniformly sampled time series instances.
         t
-            Time steps.
+            Equidistant timestamps corresponding to each input instances (i.e, the array should contain
+            numerical values in increasing order). If not provided, the timestamps will be replaced by an array of
+            integers `[0, 1, ... , N - 1]`, where `N` is the size of the input time series.
         return_instance_score
             Whether to return instance level outlier scores.
 
         Returns
         -------
-        Dictionary containing 'meta' and 'data' dictionaries.
-        'meta' has the model's metadata.
-        'data' contains the outlier predictions and instance level outlier scores.
+        Dictionary containing `meta` and `data` dictionaries.
+         - `meta` - has the model's metadata.
+         - `data` - contains the outlier predictions and instance level outlier scores.
         """
         if t is None:
             t = np.arange(X.shape[0])
 
         # compute outlier scores
-        iscore = self.score(X.reshape(-1, ), t)
+        iscore = self.score(X, t)
 
         # values above threshold are outliers
         outlier_pred = (iscore > self.threshold).astype(int)
--- alibi_detect/od/tests/test_sr.py
+++ alibi_detect/od/tests/test_sr.py
@@ -1,35 +1,31 @@
-from itertools import product
-import numpy as np
 import pytest
+import numpy as np
 from alibi_detect.od import SpectralResidual
 from alibi_detect.version import __version__
 
-# create normal time series and one with perturbations
-t = np.linspace(0, 0.5, 1000)
-X = np.sin(40 * 2 * np.pi * t) + 0.5 * np.sin(90 * 2 * np.pi * t)
-idx_pert = np.random.randint(0, 1000, 10)
-X_pert = X.copy()
-X_pert[idx_pert] = 10
 
-window_amp = [10, 20]
-window_local = [20, 30]
-n_est_points = [10, 20]
-return_instance_score = [True, False]
+@pytest.fixture(scope='module')
+def signal():
+    np.random.seed(0)
 
-tests = list(product(window_amp, window_local, n_est_points, return_instance_score))
-n_tests = len(tests)
+    # create normal time series and one with perturbations
+    t = np.linspace(0, 0.5, 1000)
+    X = np.sin(40 * 2 * np.pi * t) + 0.5 * np.sin(90 * 2 * np.pi * t)
 
-
-@pytest.fixture
-def sr_params(request):
-    return tests[request.param]
+    idx_pert = np.random.choice(np.arange(1000), size=10, replace=False)  # ensure we perturb exactly 10 points
+    X_pert = X.copy()
+    X_pert[idx_pert] = 10
+    return {"t": t, "X": X, "X_pert": X_pert}
 
 
-@pytest.mark.parametrize('sr_params', list(range(n_tests)), indirect=True)
-def test_sr(sr_params):
-    window_amp, window_local, n_est_points, return_instance_score = sr_params
+@pytest.mark.parametrize('window_amp', [10, 20])
+@pytest.mark.parametrize('window_local', [20, 30])
+@pytest.mark.parametrize('n_est_points', [10, 20])
+@pytest.mark.parametrize('return_instance_score', [True, False])
+def test_detector(signal, window_amp, window_local, n_est_points, return_instance_score):
+    t, X, X_pert = signal["t"], signal['X'], signal['X_pert']
 
-    threshold = 2.5
+    threshold = 6
     od = SpectralResidual(threshold=threshold, window_amp=window_amp,
                           window_local=window_local, n_est_points=n_est_points)
 
@@ -46,10 +42,63 @@ def test_sr(sr_params):
     else:
         assert preds_in['data']['instance_score'] is None
     preds_out = od.predict(X_pert, t, return_instance_score=return_instance_score)
-    assert preds_out['data']['is_outlier'].sum() > 0
+    assert preds_out['data']['is_outlier'].sum() >= 10  # check if we detect at least the number of perturbed points
     if return_instance_score:
         assert preds_out['data']['is_outlier'].sum() == (preds_out['data']['instance_score']
                                                          > od.threshold).astype(int).sum()
     else:
         assert preds_out['data']['instance_score'] is None
     assert preds_out['meta'] == od.meta
+
+
+@pytest.mark.parametrize('method', ['constant', 'replicate', 'reflect'])
+@pytest.mark.parametrize('side', ['left', 'right', 'bilateral'])
+def test_padding(method, side):
+    np.random.seed(0)
+
+    for _ in range(100):
+        X_size = np.random.randint(low=10, high=1000)
+        W_size = np.random.randint(low=2, high=X_size - 1)
+
+        X = np.random.randint(low=0, high=10, size=X_size)
+        W = np.random.randint(low=0, high=10, size=W_size)
+
+        X_pad = SpectralResidual.pad_same(X=X, W=W, method=method, side=side)
+        X_conv = np.convolve(X_pad, W, 'valid')
+        assert X_conv.shape[0] == X_size
+
+        # length of the padding for laterals
+        pad_right = (W_size - 1) // 2
+        pad_left = W_size - 1 - pad_right
+
+        if method == 'constant':
+            if side == 'left':
+                assert np.all(X_pad[:W_size - 1] == 0)
+            elif side == 'right':
+                assert np.all(X_pad[-W_size + 1:] == 0)
+            else:
+                if pad_left > 0:
+                    assert np.all(X_pad[:pad_left] == 0)
+                if pad_right > 0:
+                    assert np.all(X_pad[-pad_right:] == 0)
+
+        elif method == 'replicate':
+            if side == 'left':
+                assert np.all(X_pad[:W_size - 1] == X[0])
+            elif side == 'right':
+                assert np.all(X_pad[-W_size + 1:] == X[-1])
+            else:
+                if pad_left > 0:
+                    assert np.all(X_pad[:pad_left] == X[0])
+                if pad_right > 0:
+                    assert np.all(X_pad[-pad_right:] == X[-1])
+        else:
+            if side == 'left':
+                assert np.all(X_pad[:W_size - 1] == X[1:W_size][::-1])
+            elif side == 'right':
+                assert np.all(X_pad[-W_size + 1:] == X[-2:-W_size - 1:-1])
+            else:
+                if pad_left > 0:
+                    assert np.all(X_pad[:pad_left] == X[1:pad_left + 1][::-1])
+                if pad_right > 0:
+                    assert np.all(X_pad[-pad_right:] == X[-pad_right - 1:-1][::-1])
