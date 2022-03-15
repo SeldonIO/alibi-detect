aa2612a1f30a118027a51e8ed9fe010237db5c8e
Ashley Scillitoe <ashley.scillitoe@seldon.io>
2022-03-15 21:05:03 +0000

    Added pydantic validation

---
+++ alibi_detect/utils/loading.py
@@ -0,0 +1,50 @@
+# type: ignore
+from alibi_detect.version import __version__
+from alibi_detect.utils.schemas import DETECTOR_CONFIGS, DETECTOR_CONFIGS_RESOLVED, __config_spec__
+import warnings
+
+
+def validate_config(cfg: dict, resolved: bool = False) -> dict:
+    """
+    Validates a detector config dict by passing the dict to the detector's pydantic model schema.
+
+    Parameters
+    ----------
+    cfg
+        The detector config dict.
+    resolved
+        Whether the config is resolved or not. For example, if resolved=True, `x_ref` is expected to be a
+        np.ndarray, wheras if resolved=False, `x_ref` is expected to be a str.
+
+    Returns
+    -------
+    The validated config dict, with missing fields set to their default values.
+    """
+    # Get detector name
+    if 'name' in cfg:
+        detector_name = cfg['name']
+    else:
+        raise ValueError('`name` missing from config.toml.')
+
+    # Validate detector specific config
+    if detector_name in DETECTOR_CONFIGS.keys():
+        if resolved:
+            cfg = DETECTOR_CONFIGS_RESOLVED[detector_name](**cfg).dict()  # type: ignore[attr-defined]
+        else:
+            cfg = DETECTOR_CONFIGS[detector_name](**cfg).dict()  # type: ignore[attr-defined]
+    else:
+        raise ValueError('Loading the specified detector from a config.toml is not yet supported.')
+
+    # check version
+    version = cfg.pop('version', None)
+    if version is not None and version != __version__:
+        warnings.warn(f'Config is from version {version} but current version is '
+                      f'{__version__}. This may lead to breaking code or invalid results.')
+
+    # Check config specification version
+    config_spec = cfg.pop('config_spec', None)
+    if config_spec is not None and config_spec != __config_spec__:
+        warnings.warn(f'Config has specification {version} when the installed '
+                      f'alibi-detect version expects specification {__config_spec__}.'
+                      'This may lead to breaking code or invalid results.')
+    return cfg
