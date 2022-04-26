c78e07c9d1c9a72ef43772406fe82d0c924cafd1
Ashley Scillitoe <ashley.scillitoe@seldon.io>
2022-04-27 00:02:40 +0100

    Initial working pyproject.toml

--- alibi_detect/version.py
+++ alibi_detect/version.py
@@ -2,4 +2,6 @@
 # 1) we don't load dependencies by storing it in __init__.py
 # 2) we can import it in setup.py for the same reason
 # 3) we can import it into your module module
-__version__ = "0.9.2dev"
+# The version is read from the pyproject.toml file
+from importlib import metadata
+__version__ = metadata.version(__package__)
