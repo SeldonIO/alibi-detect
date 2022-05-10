973424a0fba4c647e664edb99da89400970b1a41
Ashley Scillitoe <ashley.scillitoe@seldon.io>
2022-05-10 12:07:34 +0100

    Rename version naming to avoid needing to update existing imports

--- alibi_detect/base.py
+++ alibi_detect/base.py
@@ -4,7 +4,7 @@ import json
 import numpy as np
 from typing import Dict
 
-from alibi_detect.version import version
+from alibi_detect.version import __version__
 
 DEFAULT_META = {
     "name": None,
@@ -58,7 +58,7 @@ class BaseDetector(ABC):
     def __init__(self):
         self.meta = copy.deepcopy(DEFAULT_META)
         self.meta['name'] = self.__class__.__name__
-        self.meta['version'] = version
+        self.meta['version'] = __version__
 
     def __repr__(self):
         return self.__class__.__name__
--- alibi_detect/version.py
+++ alibi_detect/version.py
@@ -1,4 +1,4 @@
 try:
-    from ._version import version  # noqa: F401
+    from ._version import version as __version__  # noqa: F401
 except ModuleNotFoundError:
     raise ImportError('alibi-detect must be installed before it can be imported.')
