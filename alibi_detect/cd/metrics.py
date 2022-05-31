6b11d588f0467e2624524d29b86bdb14736d9e21
Ashley Scillitoe <ashley.scillitoe@seldon.io>
2022-05-31 13:24:34 +0100

    Address comments

--- alibi_detect/cd/metrics.py
+++ alibi_detect/cd/metrics.py
@@ -1,11 +1,68 @@
+import os
+from pathlib import Path
 import numpy as np
-from typing import Union, List, Optional, Tuple, Callable
+from typing import Union, List, Optional, Tuple, Callable, Dict, Any
 import matplotlib.pyplot as plt
 from scipy.stats import uniform
 import statsmodels.api as sm
 import seaborn as sns
-#  import warnings
 from tqdm import tqdm
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def eval_roc(
+        detector: type,  # TODO: type w/ BaseDriftDetector if introduced.
+        X_ref: Union[np.ndarray, list],
+        X_drift: Optional[Union[np.ndarray, list]] = None,
+        C_ref: Optional[np.ndarray] = None,
+        C_drift: Optional[np.ndarray] = None,
+        model: Optional[Callable] = None,
+        detector_kwargs: Optional[dict] = None,
+        sig_levels: np.ndarray = np.linspace(0.01, 0.3, 10),
+        n_runs: int = 100,
+        n_samples: Tuple[int, int] = (500, 500),
+        qq_plot: bool = False,
+        hist_plot: bool = False,
+        power_plot: bool = False,
+        roc_plot: bool = False,
+        save_dir: Optional[os.PathLike] = None,
+        random_seed: Optional[Union[int, np.random.Generator]] = None,
+        verbose: bool = False,
+) -> Dict[str, Any]:
+    # Check data sizes
+    _check_sufficient_data_size(X_ref, n_samples, n_runs, X_test=X_drift)
+
+    # Evaluate False Positive Rates
+    if verbose:
+        print('Computing False Positive Rates...')
+    p_vals = eval_calibration(detector, X_ref, C=C_ref, model=model, detector_kwargs=detector_kwargs, sig_level=0.05,
+                              n_runs=n_runs, n_samples=n_samples, verbose=verbose, hist_plot=hist_plot, qq_plot=qq_plot,
+                              save_dir=save_dir, random_seed=random_seed)
+    FPR = [np.sum(p_vals <= sig_levels[i])/len(p_vals) for i in range(len(sig_levels))]
+    FPR = np.array(FPR)
+    results = {'FPR': FPR}
+
+    # Evaluate True Positive Rates
+    if X_drift is not None:
+        if verbose:
+            print('Computing True Positive Rates...')
+        TPR = eval_test_power(detector, X_ref=X_ref, X_drift=X_drift, C_ref=C_ref, C_drift=C_drift, model=model,
+                              detector_kwargs=detector_kwargs, sig_levels=sig_levels, n_runs=n_runs,
+                              n_samples=n_samples, power_plot=power_plot, random_seed=random_seed, verbose=verbose,
+                              save_dir=save_dir)
+        results['TPR'] = TPR
+
+        if sig_levels is not None:
+            AUC = compute_auc(FPR, TPR)
+            results['AUC'] = AUC
+
+        if roc_plot:
+            save_file = Path(save_dir).joinpath('roc_plot.png') if save_dir is not None else None
+            plot_roc(FPR, TPR, save_file=save_file)
+
+    return results
 
 
 def eval_calibration(
@@ -14,10 +71,12 @@ def eval_calibration(
         C: Optional[np.ndarray] = None,
         model: Optional[Callable] = None,
         detector_kwargs: Optional[dict] = None,
+        sig_level: float = 0.05,
         n_runs: int = 100,
-        n_samples: int = 500,
+        n_samples: Tuple[int, int] = (500, 500),
         qq_plot: bool = False,
         hist_plot: bool = False,
+        save_dir: Optional[os.PathLike] = None,
         random_seed: Optional[Union[int, np.random.Generator]] = None,
         verbose: bool = True
 ) -> np.ndarray:
@@ -38,14 +97,17 @@ def eval_calibration(
         A PyTorch, TensorFlow or Sklearn model. Should only be given for detectors accepting a `model` arg, such
         as :class:`~alibi_detect.cd.ClassifierDrift`.
     detector_kwargs
-        Keyword arguments to pass to detector.
+        Dictionary of additional arguments and keyword arguments to pass to detector. If `detector` is a
+        :class:`~alibi_detect.cd.ContextMMDDrift` type, a reservoir of context data should be passed via the `C` key.
+        If `detector` is a type requiring a `model`, such as :class:`~alibi_detect.cd.ClassifierDrift`, this should be
+        passed via the `model` field.
     n_runs
         The number of experiment runs. A larger number will give more stable p-value distributions, at the expense of
         a longer runtime.
     n_samples
         The number of samples to randomly sample from the data reservoir to create reference and test data for each run.
         The two data subsampled data sets will each contain `n_samples` instances, such that `2*n_samples` instances
-        are sampled in total.
+        are sampled in total. TODO - update
     qq_plot
         Whether to visualise the p-values on a Quantile-Quantile plot. This can also be done later with the
         :func:`~plot_qq` function.
@@ -56,19 +118,20 @@ def eval_calibration(
         Random seed (or :class:`~np.random.Generator`) to use for random sampling. If `None`, then fresh,
         unpredictable entropy will be pulled from the OS.
     verbose
-        TODO
+        Whether to print a progress bar.
 
     Returns
     -------
-    A numpy array containing the detector's p-value prediction for each of the `n_runs` number of runs.
+    A NumPy ndarray containing the detector's p-value prediction for each of the `n_runs` number of runs.
     """
     # Check data sizes
     n_data = len(X)
     _check_sufficient_data_size(X, n_samples, n_runs)
 
-    # Setup detector_kwargs
+    # Extract args and kwargs
     if detector_kwargs is None:
         detector_kwargs = {}
+    detector_kwargs.pop('p_val', None)  # Set via sig_level
 
     # Perform preprocessing (if preprocess_fn exists) to save repeated compute in loop
     preprocess_fn = detector_kwargs.pop('preprocess_fn', None)
@@ -84,7 +147,7 @@ def eval_calibration(
     for _ in runs:
         # Subsample data
         idx = rng.choice(n_data, size=n_data, replace=False)
-        idx_ref, idx_nodrift = idx[:n_samples], idx[n_samples:2*n_samples]
+        idx_ref, idx_nodrift = idx[:n_samples[0]], idx[n_samples[0]:2*n_samples[1]]
         x_ref, x_nodrift = X[idx_ref], X[idx_nodrift]
         detector_args, predict_args = [x_ref], [x_nodrift]
 
@@ -97,21 +160,30 @@ def eval_calibration(
             detector_args.append(model)
 
         # Init detector and predict
-        dd = detector(*detector_args, **detector_kwargs)
+        dd = detector(*detector_args, p_val=sig_level, **detector_kwargs)
         preds = dd.predict(*predict_args)
         p_vals_list.append(preds['data']['p_val'])
-    p_vals = np.array(p_vals_list).flatten()
-    # TODO - is above OK? (to flatten over features for univariate detectors)
+    p_vals = np.array(p_vals_list)
+
+    # Apply univariate correction (if a univariate detector)
+    correction = getattr(dd, 'correction', None)
+    if correction is not None:
+        n_features = p_vals.shape[1]
+        if correction == 'bonferroni':
+            p_vals = np.min(p_vals, axis=1)*n_features
+            p_vals = np.clip(p_vals, None, 1.0)  # Clipped as suggested by doi.org/10.1093/biomet/asaa027 (page 2)
+        elif correction == 'fdr':
+            p_vals_sorted = np.sort(p_vals, axis=1)
+            p_vals = np.min(p_vals_sorted/(np.arange(n_features)+1), axis=1)*n_features
 
     # QQ-plot
     if qq_plot:
-        plot_qq(p_vals)
+        save_file = Path(save_dir).joinpath('qq_plot.png') if save_dir is not None else None
+        plot_qq(p_vals, save_file=save_file)
     # Histogram plot
     if hist_plot:
-        plot_hist(p_vals)
-    # Show plots
-    if qq_plot or hist_plot:
-        plt.show()
+        save_file = Path(save_dir).joinpath('hist_plot.png') if save_dir is not None else None
+        plot_hist(p_vals, save_file=save_file)
 
     return p_vals
 
@@ -124,14 +196,15 @@ def eval_test_power(
     C_drift: Optional[np.ndarray] = None,
     model: Optional[Callable] = None,
     detector_kwargs: Optional[dict] = None,
-    sig_levels: np.ndarray = np.linspace(0.05, 0.5, 10),
+    sig_levels: np.ndarray = np.linspace(0.01, 0.3, 10),
     n_runs: int = 100,
     n_samples: Tuple[int, int] = (500, 500),
-    return_auc: bool = True,
     power_plot: bool = False,
+    save_dir: Optional[os.PathLike] = None,
+    return_power_auc: bool = False,
     random_seed: Optional[Union[int, np.random.Generator]] = None,
     verbose: bool = False,
-) -> Union[np.ndarray, Tuple[np.ndarray, np.ndarray]]:
+) -> np.ndarray:
     """
 
     Parameters
@@ -151,7 +224,7 @@ def eval_test_power(
     # Setup detector_kwargs
     if detector_kwargs is None:
         detector_kwargs = {}
-    detector_kwargs.pop('p_val', None)  # Need to set this ourselves
+    detector_kwargs.pop('p_val', None)  # Set via sig_levels
 
     # Perform preprocessing (if preprocess_fn exists) to save repeated compute in loop
     preprocess_fn = detector_kwargs.pop('preprocess_fn', None)
@@ -188,19 +261,20 @@ def eval_test_power(
     power /= n_runs
 
     if power_plot:
-        plot_power(sig_levels, power)
-        plt.show()
+        save_file = Path(save_dir).joinpath('power_plot.png') if save_dir is not None else None
+        plot_power(sig_levels, power, save_file=save_file)
 
-    if return_auc:
-        auc = compute_auc(sig_levels, power)
-        return power, auc
+    if return_power_auc:
+        power_auc = compute_auc(sig_levels, power)
+        return power, power_auc
 
     return power
 
 
 def plot_qq(p_vals: np.ndarray,
             title: Optional[str] = None,
-            fig: Optional[plt.Axes] = None) -> plt.Figure:
+            fig: Optional[plt.Axes] = None,
+            save_file: Optional[os.PathLike] = None) -> plt.Figure:
     """
     Plot QQ-plots of p-value to evaluate drift detector calibration.
 
@@ -231,6 +305,9 @@ def plot_qq(p_vals: np.ndarray,
     if title is not None:
         fig.suptitle(title)
 
+    if save_file is not None:
+        _save_fig(save_file)
+
     return fig
 
 
@@ -241,7 +318,8 @@ def plot_hist(
     labels: Optional[Union[str, List[str]]] = None,
     ylim: Optional[tuple] = None,
     binwidth: float = 0.05,
-    ax: Optional[plt.Axes] = None
+    ax: Optional[plt.Axes] = None,
+    save_file: Optional[os.PathLike] = None
 ) -> plt.Axes:
     """
     Plot a histogram to evaluate drift detector calibration.
@@ -266,7 +344,7 @@ def plot_hist(
     colors = [colors] if not isinstance(colors, list) else colors
     labels = [labels] if not isinstance(labels, list) else labels
     for p_val, label, color in zip(p_vals, labels, colors):
-        sns.histplot(p_val, color=color, kde=True, label=label, binwidth=binwidth, stat='probability', ax=ax)
+        sns.histplot(p_val, color=color, label=label, binwidth=binwidth, stat='probability', ax=ax)
     if label is not None:
         ax.legend(loc='upper right')
     ax.set_xlim(-0.02, 1.02)
@@ -277,6 +355,9 @@ def plot_hist(
     if title is not None:
         ax.set_title(title)
 
+    if save_file is not None:
+        _save_fig(save_file)
+
     return ax
 
 
@@ -286,8 +367,8 @@ def plot_power(
         title: Optional[str] = None,
         colors: Union[str, List[str]] = 'turquoise',
         labels: Optional[Union[str, List[str]]] = None,
-        ylim: Optional[tuple] = None,
-        ax: Optional[plt.Axes] = None
+        ax: Optional[plt.Axes] = None,
+        save_file: Optional[os.PathLike] = None
 ) -> plt.Axes:
     """
     TODO
@@ -303,19 +384,57 @@ def plot_power(
         ax.fill_between(sig_levels, power, alpha=0.2, color=color)
     if label is not None:
         ax.legend(loc='upper left')
-    ax.set_xlim(-0.02, 1.02)
-    if ylim is not None:
-        ax.set_ylim(ylim)
     if title is not None:
         ax.set_title(title)
     ax.set_xlabel(r'Significance level, $\alpha$')
     ax.set_ylabel('Test power')
 
+    if save_file is not None:
+        _save_fig(save_file)
+
     return ax
 
 
-def compute_auc(sig_levels: np.ndarray, power: np.ndarray):
-    return np.trapz(power, x=sig_levels)  # could also use scipy.integrate.simpson or sklearn.metrics.auc
+def plot_roc(
+        FPR: Union[np.ndarray, List[np.ndarray]],
+        TPR: Union[np.ndarray, List[np.ndarray]],
+        title: Optional[str] = None,
+        colors: Union[str, List[str]] = 'turquoise',
+        labels: Optional[Union[str, List[str]]] = None,
+        ax: Optional[plt.Axes] = None,
+        save_file: Optional[os.PathLike] = None
+) -> plt.Axes:
+    """
+    TODO
+    """
+    if ax is None:
+        fig, ax = plt.subplots(1, figsize=(7, 5))
+
+    FPRs = [FPR] if not isinstance(FPR, list) else FPR
+    TPRs = [TPR] if not isinstance(TPR, list) else TPR
+    colors = [colors] if not isinstance(colors, list) else colors
+    labels = [labels] if not isinstance(labels, list) else labels
+    max_val = 0.0
+    for FPR, TPR, label, color in zip(FPRs, TPRs, labels, colors):
+        sns.lineplot(x=FPR, y=TPR, marker='o', mec='k', ms=8, color=color, label=label, ax=ax)
+        ax.fill_between(FPR, TPR, alpha=0.2, color=color)
+        max_val = max(max_val, min(max(FPR), max(TPR)))
+    sns.lineplot(x=[0, max_val], y=[0, max_val], linestyle='--', color='black', alpha=0.4, ax=ax)
+    if label is not None:
+        ax.legend(loc='upper left')
+    if title is not None:
+        ax.set_title(title)
+    ax.set_xlabel('False Positive Rate')
+    ax.set_ylabel('True Positive Rate')
+
+    if save_file is not None:
+        _save_fig(save_file)
+
+    return ax
+
+
+def compute_auc(x: np.ndarray, y: np.ndarray):
+    return np.trapz(y, x=x)  # could also use scipy.integrate.simpson or sklearn.metrics.auc
 
 
 def _check_sufficient_data_size(X: Union[np.ndarray, list],
@@ -337,3 +456,14 @@ def _check_sufficient_data_size(X: Union[np.ndarray, list],
 #        if len(X_test) / n_samples[1] < FACT*n_runs:
 #            warnings.warn('The size of the provided test set might not be large enough to give reliable '
 #                          'statistics.')
+
+
+def _save_fig(save_file: os.PathLike):
+    # Create directory if needed
+    save_dir = Path(save_file).parent
+    if save_dir is not None:
+        if not save_dir.is_dir():
+            logger.warning('Directory {} does not exist and is now created.'.format(save_dir))
+            save_dir.mkdir(parents=True, exist_ok=True)
+    # Save figure
+    plt.savefig(save_file)
