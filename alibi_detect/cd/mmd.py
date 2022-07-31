753ba72c27edc2f4a32c1c51c23a358384d0d316
Hao Song <nuacesh@gmail.com>
2022-07-31 15:49:09 +0100

    Fixed prediction behaviour for torch gpu with new base kernel.

--- alibi_detect/cd/mmd.py
+++ alibi_detect/cd/mmd.py
@@ -23,7 +23,7 @@ class MMDDrift:
             preprocess_x_ref: bool = True,
             update_x_ref: Optional[Dict[str, int]] = None,
             preprocess_fn: Optional[Callable] = None,
-            kernel: Union[BaseKernelTorch, BaseKernelTF] = None,
+            kernel: Callable = None,
             # sigma: Optional[np.ndarray] = None,
             configure_kernel_from_x_ref: bool = True,
             n_permutations: int = 100,
