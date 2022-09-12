347aa0f20150bc089958aad230bf4118ea211942
Ashley Scillitoe <ashley.scillitoe@seldon.io>
2022-09-12 13:23:05 +0100

    Fix ci.yml 3rd time, plus fix to legacy saving __init__

--- alibi_detect/utils/saving/__init__.py
+++ alibi_detect/utils/saving/__init__.py
@@ -1,1 +1,6 @@
-from alibi_detect.utils.saving.saving import save_detector, load_detector  # noqa
+from alibi_detect.utils.saving.saving import save_detector, load_detector
+
+__all__ = [
+    "save_detector",
+    "load_detector"
+]
