eb344182dd9f3fee0ca4d30f89f2bb558724869d
Ashley Scillitoe <ashley.scillitoe@seldon.io>
2022-09-12 14:19:45 +0100

    Remove changes to legacy saving init

--- alibi_detect/utils/saving/__init__.py
+++ alibi_detect/utils/saving/__init__.py
@@ -1,6 +1,1 @@
-from alibi_detect.utils.saving.saving import save_detector, load_detector
-
-__all__ = [
-    "save_detector",
-    "load_detector"
-]
+from alibi_detect.utils.saving.saving import save_detector, load_detector  # noqa
