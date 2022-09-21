e8e40896c8f857bcb407efbcf87995a05962750a
Janis Klaise <jk@seldon.io>
2022-09-21 17:41:15 +0100

    Add upper version bound and update `all` bucket

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ exec(open("alibi_detect/version.py").read())
 
 extras_require = {
     "prophet": [
-        "prophet>=1.1",
+        "prophet>=1.1.0, <2.0.0",
     ],
     "torch": [
         "torch>=1.7.0, <1.13.0"
@@ -26,9 +26,7 @@ extras_require = {
         "torch>=1.7.0, <1.13.0"
     ],
     "all": [
-        "fbprophet>=0.5, <0.7",
-        "holidays==0.9.11",
-        "pystan<3.0",
+        "prophet>=1.1.0, <2.0.0",
         "tensorflow_probability>=0.8.0, <0.18.0",
         "tensorflow>=2.2.0, !=2.6.0, !=2.6.1, <2.10.0",  # https://github.com/SeldonIO/alibi-detect/issues/375 and 387
         "pykeops>=2.0.0, <2.2.0",
