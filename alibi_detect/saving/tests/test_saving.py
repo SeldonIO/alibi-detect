b70f5b48d9257e74ab847b266a93be4ac988a6c0
Ashley Scillitoe <ashley.scillitoe@seldon.io>
2022-09-22 11:43:46 +0100

    Check that dir contains a SavedModel in test_save_model

--- alibi_detect/saving/tests/test_saving.py
+++ alibi_detect/saving/tests/test_saving.py
@@ -1190,7 +1190,7 @@ def test_save_model(data, model, layer, backend, tmp_path):
     cfg_model = _path2str(cfg_model)
     cfg_model = ModelConfig(**cfg_model).dict()
     assert tmp_path.joinpath('model').is_dir()
-#    assert tmp_path.joinpath('model/model.h5').is_file()
+    assert tf.saved_model.contains_saved_model(tmp_path.joinpath('model'))
 
     # Adjust config
     cfg_model['src'] = tmp_path.joinpath('model')  # Need to manually set to absolute path here
