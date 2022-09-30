c6e78fcc28652f54fb71fce46e2ad189c907b645
Ashley Scillitoe <ashley.scillitoe@seldon.io>
2022-09-30 15:22:01 +0100

    Remove config_spec versioning (#641)

--- alibi_detect/version.py
+++ alibi_detect/version.py
@@ -3,9 +3,3 @@
 # 2) we can import it in setup.py for the same reason
 # 3) we can import it into your module module
 __version__ = "0.10.4dev"
-
-# Define the config specification version. This is distinct to the library version above. It is only updated when
-# any detector config schema is updated, such that loading a previous config spec cannot be guaranteed to work.
-# The minor version number is associated with minor changes such as adding/removing/changing kwarg's, whilst the major
-# number is reserved for significant changes to the config layout.
-__config_spec__ = "0.1"
