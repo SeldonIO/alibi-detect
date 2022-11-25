e272c74f91a4361d78f37e6f36d897b374f5a489
Alex Athorne <alex.athorne@seldon.io>
2022-11-25 10:16:24 +0000

    Make knn tests better

--- alibi_detect/od/tests/test_knn.py
+++ alibi_detect/od/tests/test_knn.py
@@ -1,40 +1,76 @@
 import pytest
 import numpy as np
+import torch
 
 from alibi_detect.od.knn import KNN
 from alibi_detect.od.backend import AverageAggregatorTorch, TopKAggregatorTorch, MaxAggregatorTorch, \
     MinAggregatorTorch, ShiftAndScaleNormaliserTorch, PValNormaliserTorch
 
 
-def test_knn_single():
+def make_knn_detector(k=5, aggregator=None, normaliser=None):
+    knn_detector = KNN(k=k, aggregator=aggregator, normaliser=normaliser)
+    x_ref = np.random.randn(100, 2)
+    knn_detector.fit(x_ref)
+    knn_detector.infer_threshold(x_ref, 0.1)
+    return knn_detector
+
+
+def test_unfitted_knn_single_score():
+    knn_detector = KNN(k=10)
+    x = np.array([[0, 10], [0.1, 0]])
+    with pytest.raises(ValueError) as err:
+        _ = knn_detector.predict(x)
+    assert str(err.value) == 'KNNTorch has not been fit!'
+
+
+def test_fitted_knn_single_score():
     knn_detector = KNN(k=10)
     x_ref = np.random.randn(100, 2)
     knn_detector.fit(x_ref)
-    x = np.array([[0, 10]])
-    assert knn_detector.predict(x)['scores'] > 5
+    x = np.array([[0, 10], [0.1, 0]])
+    y = knn_detector.predict(x)
+    assert y['scores'][0] > 5
+    assert y['scores'][1] < 1
 
-    x = np.array([[0, 0.1]])
-    assert knn_detector.predict(x)['scores'] < 1
+    assert not y['threshold_inferred']
+    assert y['threshold'] is None
+    assert y['preds'] is None
+    assert y['p_vals'] is None
 
+
+def test_fitted_knn_predict():
+    knn_detector = make_knn_detector(k=10)
+    x_ref = np.random.randn(100, 2)
     knn_detector.infer_threshold(x_ref, 0.1)
-
-    x = np.array([[0, 10]])
-    pred = knn_detector.predict(x)
-    assert pred['scores'] > 5
-    assert pred['preds']
-    assert pred['p_vals'] < 0.05
-
-    x = np.array([[0, 0.1]])
-    pred = knn_detector.predict(x)
-    assert pred['scores'] < 1
-    assert not pred['preds']
-    assert pred['p_vals'] > 0.7
+    x = np.array([[0, 10], [0, 0.1]])
+    y = knn_detector.predict(x)
+    assert y['scores'][0] > 5
+    assert y['scores'][1] < 1
+    assert y['threshold_inferred']
+    assert y['threshold'] is not None
+    assert y['p_vals'].all()
+    assert (y['preds'] == [True, False]).all()
 
 
 @pytest.mark.parametrize("aggregator", [AverageAggregatorTorch, lambda: TopKAggregatorTorch(k=7),
                                         MaxAggregatorTorch, MinAggregatorTorch])
 @pytest.mark.parametrize("normaliser", [ShiftAndScaleNormaliserTorch, PValNormaliserTorch, lambda: None])
-def test_knn_ensemble(aggregator, normaliser):
+def test_unfitted_knn_ensemble(aggregator, normaliser):
+    knn_detector = KNN(
+        k=[8, 9, 10],
+        aggregator=aggregator(),
+        normaliser=normaliser()
+    )
+    x = np.array([[0, 10], [0.1, 0]])
+    with pytest.raises(ValueError) as err:
+        _ = knn_detector.predict(x)
+    assert str(err.value) == 'KNNTorch has not been fit!'
+
+
+@pytest.mark.parametrize("aggregator", [AverageAggregatorTorch, lambda: TopKAggregatorTorch(k=7),
+                                        MaxAggregatorTorch, MinAggregatorTorch])
+@pytest.mark.parametrize("normaliser", [ShiftAndScaleNormaliserTorch, PValNormaliserTorch, lambda: None])
+def test_fitted_knn_ensemble(aggregator, normaliser):
     knn_detector = KNN(
         k=[8, 9, 10],
         aggregator=aggregator(),
@@ -43,10 +79,29 @@ def test_knn_ensemble(aggregator, normaliser):
     x_ref = np.random.randn(100, 2)
     knn_detector.fit(x_ref)
     x = np.array([[0, 10], [0, 0.1]])
-    knn_detector.infer_threshold(x_ref, 0.1)
-    pred = knn_detector.predict(x)
+    y = knn_detector.predict(x)
+    assert y['scores'].all()
+    assert not y['threshold_inferred']
+    assert y['threshold'] is None
+    assert y['preds'] is None
+    assert y['p_vals'] is None
 
-    assert np.all(pred['preds'] == [True, False])
+
+@pytest.mark.parametrize("aggregator", [AverageAggregatorTorch, lambda: TopKAggregatorTorch(k=7),
+                                        MaxAggregatorTorch, MinAggregatorTorch])
+@pytest.mark.parametrize("normaliser", [ShiftAndScaleNormaliserTorch, PValNormaliserTorch, lambda: None])
+def test_fitted_knn_ensemble_predict(aggregator, normaliser):
+    knn_detector = make_knn_detector(
+        k=[8, 9, 10],
+        aggregator=aggregator(),
+        normaliser=normaliser()
+    )
+    x = np.array([[0, 10], [0, 0.1]])
+    y = knn_detector.predict(x)
+    assert y['threshold_inferred']
+    assert y['threshold'] is not None
+    assert y['p_vals'].all()
+    assert (y['preds'] == [True, False]).all()
 
 
 def test_incorrect_knn_ensemble_init():
@@ -54,3 +109,22 @@ def test_incorrect_knn_ensemble_init():
         KNN(k=[8, 9, 10])
     assert str(err.value) == ("k=[8, 9, 10] is type <class 'list'> but aggregator is None, you must specify at least an"
                               " aggregator if you want to use the knn detector ensemble like this.")
+
+
+@pytest.mark.parametrize("aggregator", [AverageAggregatorTorch, lambda: TopKAggregatorTorch(k=7),
+                                        MaxAggregatorTorch, MinAggregatorTorch])
+@pytest.mark.parametrize("normaliser", [ShiftAndScaleNormaliserTorch, PValNormaliserTorch, lambda: None])
+def test_knn_ensemble_torch_script(aggregator, normaliser):
+    knn_detector = make_knn_detector(k=[5, 6, 7], aggregator=aggregator(), normaliser=normaliser())
+    tsknn = torch.jit.script(knn_detector.backend)
+    x = torch.tensor([[0, 10], [0, 0.1]])
+    y = tsknn(x)
+    assert torch.all(y == torch.tensor([True, False]))
+
+
+def test_knn_single_torchscript():
+    knn_detector = make_knn_detector(k=5)
+    tsknn = torch.jit.script(knn_detector.backend)
+    x = torch.tensor([[0, 10], [0, 0.1]])
+    y = tsknn(x)
+    assert torch.all(y == torch.tensor([True, False]))
