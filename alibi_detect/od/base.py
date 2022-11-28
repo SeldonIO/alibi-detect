e884e9447dbf164cf1446c5cd12dd551f45cd995
Alex Athorne <alex.athorne@seldon.io>
2022-11-28 16:26:42 +0000

    Add docstrings for outlier detector base class

--- alibi_detect/od/base.py
+++ alibi_detect/od/base.py
@@ -10,39 +10,86 @@ logger = logging.getLogger(__name__)
 
 
 class OutlierDetectorOutput(TypedDict):
+    """Outlier detector output type definition."""
     meta: Dict[str, str]
     data: Dict[str, Union[bool, np.ndarray]]
 
 
 class OutlierDetector(BaseDetector, ABC):
-    """ Base class for outlier detection algorithms. """
     threshold_inferred = False
     ensemble = False
 
     def __init__(self):
+        """ Base class for outlier detection algorithms."""
         super().__init__()
         self.meta['online'] = False
         self.meta['detector_type'] = 'outlier'
 
     @abstractmethod
     def fit(self, X: np.ndarray) -> None:
+        """
+        Fit outlier detector to data.
+
+        Parameters
+        ----------
+        X
+            Reference data.
+        """
         pass
 
     @abstractmethod
     def score(self, X: np.ndarray) -> np.ndarray:
+        """
+        Compute anomaly scores of the instances in X.
+
+        Parameters
+        ----------
+        X
+            Data to score.
+
+        Returns
+        -------
+        Anomaly scores. The higher the score, the more anomalous the instance.
+        """
         pass
 
     @abstractmethod
     def infer_threshold(self, X: np.ndarray, fpr: float) -> None:
+        """
+        Infer the threshold for the outlier detector.
+
+        Parameters
+        ----------
+        X
+            Reference data.
+        fpr
+            False positive rate used to infer the threshold.
+        """
         pass
 
     @abstractmethod
     def predict(self, X: np.ndarray) -> OutlierDetectorOutput:
+        """
+        Predict whether the instances in X are outliers or not.
+
+        Parameters
+        ----------
+        X
+            Data to predict.
+ 
+        Returns
+        -------
+        Dict with keys 'data' and 'meta'. 'data' contains the outlier scores. If threshold inference was performed,
+        'data' also contains the outlier labels.
+        """
         pass
 
 
+# Using Protocols instead base classes for the backend classes. This is a bit more flexible and allows us to
+# avoid the torch/tensorflow imports in the base class.
 @runtime_checkable
 class TransformProtocol(Protocol):
+    """Protocol for transformer objects."""
     def transform(self, X):
         pass
 
@@ -52,6 +99,7 @@ class TransformProtocol(Protocol):
 
 @runtime_checkable
 class FittedTransformProtocol(TransformProtocol, Protocol):
+    """Protocol for fitted transformer objects."""
     def fit(self, x_ref):
         pass
 
