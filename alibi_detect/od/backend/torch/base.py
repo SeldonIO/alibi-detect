6ca806248e46a7b98666c63c897520a40459480b
Alex Athorne <alex.athorne@seldon.io>
2022-11-28 15:27:51 +0000

    Add docstrings for base torch outlier detector class

--- alibi_detect/od/backend/torch/base.py
+++ alibi_detect/od/backend/torch/base.py
@@ -13,6 +13,7 @@ logger = logging.getLogger(__name__)
 
 @dataclass
 class TorchOutlierDetectorOutput:
+    """Output of the outlier detector."""
     threshold_inferred: bool
     scores: torch.Tensor
     threshold: Optional[torch.Tensor]
@@ -20,6 +21,7 @@ class TorchOutlierDetectorOutput:
     p_vals: Optional[torch.Tensor]
 
     def numpy(self) -> Dict[str, Union[bool, Optional[torch.Tensor]]]:
+        """Converts the output to numpy."""
         outputs = asdict(self)
         for key, value in outputs.items():
             if isinstance(value, torch.Tensor):
@@ -28,7 +30,7 @@ class TorchOutlierDetectorOutput:
 
 
 class TorchOutlierDetector(torch.nn.Module, FitMixinTorch, ABC):
-    """ Base class for torch backend outlier detection algorithms."""
+    """Base class for torch backend outlier detection algorithms."""
     threshold_inferred = False
     threshold = None
 
@@ -37,38 +39,153 @@ class TorchOutlierDetector(torch.nn.Module, FitMixinTorch, ABC):
 
     @abstractmethod
     def _fit(self, x_ref: torch.Tensor) -> None:
+        """Fit the outlier detector to the reference data.
+
+        Parameters
+        ----------
+        x_ref
+            Reference data.
+
+        Raises
+        ------
+        `NotImplementedError`
+            Raised if not implemented.
+        """
         raise NotImplementedError()
 
     @abstractmethod
     def score(self, X: torch.Tensor) -> torch.Tensor:
+        """Score the data.
+
+        Parameters
+        ----------
+        X
+            Data to score.
+
+        Raises
+        ------
+        `NotImplementedError`
+            Raised if not implemented.
+        """
         raise NotImplementedError()
 
     @torch.jit.ignore
     def check_threshould_infered(self):
+        """Check if threshold is inferred.
+
+        Raises
+        ------
+        ValueError
+            Raised if threshold is not inferred.
+        """
         if not self.threshold_inferred:
             raise ValueError((f'{self.__class__.__name__} has no threshold set, '
                               'call `infer_threshold` before predicting.'))
 
     def _to_tensor(self, X: Union[List, np.ndarray]):
+        """Converts the data to a tensor.
+
+        Parameters
+        ----------
+        X
+            Data to convert.
+
+        Returns
+        -------
+        `torch.Tensor`
+        """
         return torch.as_tensor(X, dtype=torch.float32)
 
     def _accumulator(self, X: torch.Tensor) -> torch.Tensor:
+        """Accumulates the data.
+
+        Parameters
+        ----------
+        X
+            Data to accumulate.
+
+        Returns
+        -------
+        `torch.Tensor` or just returns original data
+        """
+        # `type: ignore` here becuase self.accumulator here causes an error with mypy when using torch.jit.script.
+        # For some reason it thinks self.accumulator is a torch.Tensor and therefore is not callable.
         return self.accumulator(X) if self.accumulator is not None else X  # type: ignore
 
     def _classify_outlier(self, scores: torch.Tensor) -> torch.Tensor:
+        """Classify the data as outlier or not.
+
+        Parameters
+        ----------
+        scores
+            Scores to classify. Larger scores indicate more likely outliers.
+
+        Returns
+        -------
+        `torch.Tensor` or `None`
+        """
         return scores > self.threshold if self.threshold_inferred else None
 
     def _p_vals(self, scores: torch.Tensor) -> torch.Tensor:
+        """Compute p-values for the scores.
+
+        Parameters
+        ----------
+        scores
+            Scores to compute p-values for.
+
+        Returns
+        -------
+        `torch.Tensor` or `None`
+        """
         return (1 + (scores[:, None] < self.val_scores).sum(-1))/len(self.val_scores) \
             if self.threshold_inferred else None
 
     def infer_threshold(self, X: torch.Tensor, fpr: float) -> None:
+        """Infer the threshold for the data. Prerequisite for outlier predictions.
+
+        Parameters
+        ----------
+        X
+            Data to infer the threshold for.
+        fpr
+            False positive rate to use for threshold inference.
+
+        Raises
+        ------
+        ValueError
+            Raised if fpr is not in (0, 1).
+        """
+        if not 0 < fpr < 1:
+            ValueError('fpr must be in (0, 1).')
         self.val_scores = self.score(X)
         self.val_scores = self._accumulator(self.val_scores)
         self.threshold = torch.quantile(self.val_scores, 1-fpr)
         self.threshold_inferred = True
 
     def predict(self, X: torch.Tensor) -> TorchOutlierDetectorOutput:
+        """Predict outlier labels for the data.
+
+        Computes the outlier scores. If the detector is not fit on reference data we raise an error.
+        If the threshold is inferred, the outlier labels and p-values are also computed and returned.
+        Otherwise, the outlier labels and p-values are set to `None`.
+
+        Parameters
+        ----------
+        X
+            Data to predict.
+
+        Raises
+        ------
+        ValueError
+            Raised if the detector is not fit on reference data.
+
+        Returns
+        -------
+        `TorchOutlierDetectorOutput`
+            Output of the outlier detector.
+
+        """
         self.check_fitted()  # type: ignore
         raw_scores = self.score(X)
         scores = self._accumulator(raw_scores)
