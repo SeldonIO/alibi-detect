7363cb531e73c7ff78c8fb015ba1cdd8694029af
Alex Athorne <alex.athorne@seldon.io>
2022-11-28 14:50:52 +0000

    Add docstrings for backend ensemble and knn objects

--- alibi_detect/od/backend/torch/ensemble.py
+++ alibi_detect/od/backend/torch/ensemble.py
@@ -14,12 +14,12 @@ logger = logging.getLogger(__name__)
 
 
 class BaseTransformTorch(Module, ABC):
-    """Base Transform class.
-
-    provides abstract methods for transform objects that map a numpy
-    array.
-    """
     def __init__(self):
+        """Base Transform class.
+
+        provides abstract methods for transform objects that map a numpy
+        array.
+        """
         super().__init__()
 
     def transform(self, X: torch.Tensor):
@@ -47,13 +47,13 @@ class BaseTransformTorch(Module, ABC):
 
 
 class FitMixinTorch:
-    """Fit mixin
-
-    Utility class that provides fitted checks for alibi-detect objects that require to be fit before use.
-    """
     _fitted = False
 
     def __init__(self):
+        """Fit mixin
+
+        Utility class that provides fitted checks for alibi-detect objects that require to be fit before use.
+        """
         super().__init__()
 
     def fit(self, X: torch.Tensor) -> FitMixinTorch:
@@ -62,48 +62,101 @@ class FitMixinTorch:
         return self
 
     def _fit(self, X: torch.Tensor):
+        """Fit on `X` tensor.
+
+        This method should be overidden on child classes.
+
+        Parameters
+        ----------
+        X
+            Reference `torch.Tensor` for fitting object.
+
+        Raises
+        ------
+        NotImplementedError
+            Raised if unimplimented.
+        """
         raise NotImplementedError()
 
     @torch.jit.ignore
     def check_fitted(self):
+        """Raises error if parent object instance has not been fit.
+
+        Raises
+        ------
+        ValueError
+            Raised if method called and object has not been fit.
+        """
         if not self._fitted:
             # TODO: make our own NotFitted Error here!
             raise ValueError(f'{self.__class__.__name__} has not been fit!')
 
 
 class BaseFittedTransformTorch(BaseTransformTorch, FitMixinTorch):
-    """Base Fitted Transform class.
-
-    Provides abstract methods for transforms that have an aditional
-    fit step.
-    """
-
     def __init__(self):
+        """Base Fitted Transform class.
+
+        Extends BaseTransfrom with fit functionality. Ensures that transform has been fit prior to
+        applying transform.
+        """
         BaseTransformTorch.__init__(self)
         FitMixinTorch.__init__(self)
 
     def transform(self, X: torch.Tensor):
+        """Checks to make sure transform has been fitted and then applies trasform to input tensor.
+
+        Parameters
+        ----------
+        X
+            `torch.Tensor` being transformed.
+
+        Returns
+        -------
+        transformed `torch.Tensor`.
+        """
         self.check_fitted()
         return self._transform(X)
 
 
 class PValNormaliser(BaseFittedTransformTorch):
-    """Maps scores to there p values.
-
-    Needs to be fit on a reference dataset using fit. Transform counts the number of scores
-    in the reference dataset that are greter than the score of interest and divides by the
-    size of the reference dataset. Output is between 1 and 0. Small values are likely to be
-    outliers.
-    """
     def __init__(self):
+        """Maps scores to there p values.
+
+        Needs to be fit (see py:obj:alibi_detect.od.backend.torch.ensemble.BaseFittedTransformTorch).
+        Transform counts the number of scores in the reference dataset that are greter than the score
+        of interest and divides by the size of the reference dataset. Output is between 1 and 0. Small
+        values are likely to be outliers.
+        """
         super().__init__()
         self.val_scores = None
 
     def _fit(self, val_scores: torch.Tensor) -> PValNormaliser:
+        """Fit transform on scores.
+
+        Parameters
+        ----------
+        val_scores
+            score outputs of ensemble of detectors applied to reference data.
+
+        Returns
+        -------
+        self
+        """
         self.val_scores = val_scores
         return self
 
     def _transform(self, scores: torch.Tensor) -> torch.Tensor:
+        """Transform scores to 1 - p values.
+
+        Parameters
+        ----------
+        scores
+            `Torch.Tensor` of scores from ensemble of detectors.
+
+        Returns
+        -------
+        `Torch.Tensor` of 1 - p values.
+        """
         p_vals = (
                 1 + (scores[:, None, :] < self.val_scores[None, :, :]).sum(1)
             )/(len(self.val_scores)+1)
@@ -111,22 +164,44 @@ class PValNormaliser(BaseFittedTransformTorch):
 
 
 class ShiftAndScaleNormaliser(BaseFittedTransformTorch):
-    """Maps scores to their normalised values.
-
-    Needs to be fit on a reference dataset using fit. Subtracts the dataset mean and
-    scales by the standard deviation.
-    """
     def __init__(self):
+        """Maps scores to their normalised values.
+
+        Needs to be fit (see py:obj:alibi_detect.od.backend.torch.ensemble.BaseFittedTransformTorch).
+        Subtracts the dataset mean and scales by the standard deviation.
+        """
         super().__init__()
         self.val_means = None
         self.val_scales = None
 
     def _fit(self, val_scores: torch.Tensor) -> ShiftAndScaleNormaliser:
+        """Computes the mean and standard deviation of the scores and stores them.
+
+        Parameters
+        ----------
+        val_scores
+            `Torch.Tensor` of scores from ensemble of detectors.
+
+        Returns
+        -------
+        self
+        """
         self.val_means = val_scores.mean(0)[None, :]
         self.val_scales = val_scores.std(0)[None, :]
         return self
 
     def _transform(self, scores: torch.Tensor) -> torch.Tensor:
+        """Transform scores to normalised values. Subtracts the mean and scales by the standard deviation.
+
+        Parameters
+        ----------
+        scores
+            `Torch.Tensor` of scores from ensemble of detectors.
+
+        Returns
+        -------
+        `Torch.Tensor` of normalised scores.
+        """
         return (scores - self.val_means)/self.val_scales
 
 
@@ -144,6 +219,17 @@ class TopKAggregator(BaseTransformTorch):
         self.k = k
 
     def _transform(self, scores: torch.Tensor) -> torch.Tensor:
+        """Takes the mean of the top k scores.
+
+        Parameters
+        ----------
+        scores
+            `Torch.Tensor` of scores from ensemble of detectors.
+
+        Returns
+        -------
+        `Torch.Tensor` of mean of top k scores.
+        """
         if self.k is None:
             self.k = int(np.ceil(scores.shape[1]/2))
         sorted_scores, _ = torch.sort(scores, 1)
@@ -151,18 +237,31 @@ class TopKAggregator(BaseTransformTorch):
 
 
 class AverageAggregator(BaseTransformTorch):
-    """Averages the scores of the detectors in an ensemble.
-
-    Parameters
-    ----------
-    weights
-        Optional parameter to weight the scores.
-    """
     def __init__(self, weights: Optional[torch.Tensor] = None):
+        """Averages the scores of the detectors in an ensemble.
+
+        Parameters
+        ----------
+        weights
+            Optional parameter to weight the scores. If `weights` is left `None` then will be set to
+            a vector of ones.
+        """
         super().__init__()
         self.weights = weights
 
     def _transform(self, scores: torch.Tensor) -> torch.Tensor:
+        """Averages the scores of the detectors in an ensemble. If weights where passed in the init
+        then these are used to weight the scores.
+
+        Parameters
+        ----------
+        scores
+            `Torch.Tensor` of scores from ensemble of detectors.
+
+        Returns
+        -------
+        `Torch.Tensor` of mean of scores.
+        """
         if self.weights is None:
             m = scores.shape[-1]
             self.weights = torch.ones(m)/m
@@ -170,41 +269,61 @@ class AverageAggregator(BaseTransformTorch):
 
 
 class MaxAggregator(BaseTransformTorch):
-    """Takes the max score of a set of detectors in an ensemble."""
     def __init__(self):
+        """Takes the maximum of the scores of the detectors in an ensemble."""
         super().__init__()
 
     def _transform(self, scores: torch.Tensor) -> torch.Tensor:
+        """Takes the max score of a set of detectors in an ensemble.
+
+        Parameters
+        ----------
+        scores
+            `Torch.Tensor` of scores from ensemble of detectors.
+
+        Returns
+        -------
+        `Torch.Tensor` of max of scores.
+        """
         vals, _ = torch.max(scores, dim=-1)
         return vals
 
 
 class MinAggregator(BaseTransformTorch):
-    """Takes the min score of a set of detectors in an ensemble."""
     def __init__(self):
+        """Takes the min score of a set of detectors in an ensemble."""
         super().__init__()
 
     def _transform(self, scores: torch.Tensor) -> torch.Tensor:
+        """Takes the min score of a set of detectors in an ensemble.
+
+        Parameters
+        ----------
+        scores
+            `Torch.Tensor` of scores from ensemble of detectors.
+
+        Returns
+        -------
+        `Torch.Tensor` of min of scores.
+        """
         vals, _ = torch.min(scores, dim=-1)
         return vals
 
 
 class Accumulator(BaseFittedTransformTorch):
     def __init__(self,
-                 normaliser: BaseFittedTransformTorch = None,
+                 normaliser: Optional[BaseFittedTransformTorch] = None,
                  aggregator: BaseTransformTorch = AverageAggregator()):
-        """Wraps a normaliser and aggregator into a single object.
-
-        The accumulator wraps normalisers and aggregators into a single object.
+        """Accumulates the scores of the detectors in an ensemble. Can be used to normalise and aggregate
+        the scores from an ensemble of detectors.
 
         Parameters
         ----------
         normaliser
-            normaliser that's an instance of BaseFittedTransformTorch. Maps the outputs of
-            a set of detectors to a common range.
+            `BaseFittedTransformTorch` object to normalise the scores. If `None` then no normalisation
+            is applied.
         aggregator
-            aggregator extendng BaseTransformTorch. Maps outputs of the normaliser to
-            single score.
+            `BaseTransformTorch` object to aggregate the scores.
         """
         super().__init__()
         self.normaliser = normaliser
@@ -213,11 +332,29 @@ class Accumulator(BaseFittedTransformTorch):
         self.aggregator = aggregator
 
     def _transform(self, X: torch.Tensor):
+        """Apply the normaliser and aggregator to the scores.
+
+        Parameters
+        ----------
+        X
+            `Torch.Tensor` of scores from ensemble of detectors.
+
+        Returns
+        -------
+        `Torch.Tensor` of aggregated and normalised scores.
+        """
         if self.normaliser is not None:
             X = self.normaliser(X)
         X = self.aggregator(X)
         return X
 
     def _fit(self, X: torch.Tensor):
+        """Fit the normaliser to the scores.
+
+        Parameters
+        ----------
+        X
+            `Torch.Tensor` of scores from ensemble of detectors.
+        """
         if self.normaliser is not None:
             self.normaliser.fit(X)
--- alibi_detect/od/backend/torch/knn.py
+++ alibi_detect/od/backend/torch/knn.py
@@ -12,20 +12,69 @@ class KNNTorch(TorchOutlierDetector):
             kernel: Optional[torch.nn.Module] = None,
             accumulator: Optional[Accumulator] = None
             ):
+        """PyTorch backend for KNN detector.
+
+        Parameters
+        ----------
+        k
+            Number of neirest neighbors to compute distance to. `k` can be a single value or
+            an array of integers. If `k` is a single value the outlier score is the distance/kernel
+            similarity to the `k`-th nearest neighbor. If `k` is a list then it returns the distance/kernel
+            similarity to each of the specified `k` neighbors.
+        kernel
+            If a kernel is specified then instead of using torch.cdist we compute the kernel similarity
+            while computing the k nearest neighbor distance.
+        accumulator
+            If `k` is an array of integers then the accumulator must not be None. Should be an instance
+            of :py:obj:`alibi_detect.od.backend.torch.ensemble.Accumulator`. Responsible for combining 
+            multiple scores into a single score.
+        """
         TorchOutlierDetector.__init__(self)
         self.kernel = kernel
         self.ensemble = isinstance(k, (np.ndarray, list, tuple))
         self.ks = torch.tensor(k) if self.ensemble else torch.tensor([k])
         self.accumulator = accumulator
 
-    def forward(self, X):
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        """Detect if X is an outlier.
+
+        Parameters
+        ----------
+        X
+            `torch.Tensor` with leading batch dimension.
+
+        Returns
+        -------
+        `torch.Tensor` of `bool` values with leading batch dimension.
+
+        Raises
+        ------
+        ValueError
+            If called before detector has had threshould_infered method called.
+        """
         raw_scores = self.score(X)
         scores = self._accumulator(raw_scores)
         self.check_threshould_infered()
         preds = scores > self.threshold
         return preds.cpu()
 
-    def score(self, X):
+    def score(self, X: torch.Tensor) -> torch.Tensor:
+        """Computes the score of `X`
+
+        Parameters
+        ----------
+        X
+            Score a tensor of instances. First dimesnion corresponds to batch. 
+
+        Returns
+        -------
+            Tensor of scores for each element in `X`.
+
+        Raises
+        ------
+        ValueError
+            If called before detector has been fit.
+        """
         self.check_fitted()
         K = -self.kernel(X, self.x_ref) if self.kernel is not None else torch.cdist(X, self.x_ref)
         bot_k_dists = torch.topk(K, torch.max(self.ks), dim=1, largest=False)
@@ -33,6 +82,13 @@ class KNNTorch(TorchOutlierDetector):
         return all_knn_dists if self.ensemble else all_knn_dists[:, 0]
 
     def _fit(self, x_ref: torch.Tensor):
+        """Fits the detector
+
+        Parameters
+        ----------
+        x_ref
+            The Dataset tensor.
+        """
         self.x_ref = x_ref
         if self.accumulator is not None:
             scores = self.score(x_ref)
