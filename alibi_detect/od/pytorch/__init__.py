babc083317489c0e65b7b182727035fe76865bda
Alex Athorne <alex.athorne@seldon.io>
2023-01-17 13:07:58 +0000

    Add MahalanobisTorch to test_dep_management tests

--- alibi_detect/od/pytorch/__init__.py
+++ alibi_detect/od/pytorch/__init__.py
@@ -1,4 +1,5 @@
 from alibi_detect.utils.missing_optional_dependency import import_optional
 
 KNNTorch = import_optional('alibi_detect.od.pytorch.knn', ['KNNTorch'])
+MahalanobisTorch = import_optional('alibi_detect.od.pytorch.mahalanobis', ['MahalanobisTorch'])
 Accumulator = import_optional('alibi_detect.od.pytorch.ensemble', ['Accumulator'])
