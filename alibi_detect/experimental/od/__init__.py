27230e36fe6ca74e369cc338ead39ce5a0d200cb
Alex Athorne <alex.athorne@seldon.io>
2023-02-01 14:34:26 +0000

    Add GMM to experimental namespace

--- alibi_detect/experimental/od/__init__.py
+++ alibi_detect/experimental/od/__init__.py
@@ -1,3 +1,4 @@
 from alibi_detect.od._knn import KNN # noqa F401
 from alibi_detect.od._mahalanobis import Mahalanobis # noqa F401
 from alibi_detect.od._pca import PCA # noqa F401
+from alibi_detect.od._gmm import GMM # noqa F401
