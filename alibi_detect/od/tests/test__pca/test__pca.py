67cb6706b31f21ad106a750107a22514a36c048b
Alex Athorne <alex.athorne@seldon.io>
2023-04-19 17:17:46 +0100

    Update tests and add docstrings

--- alibi_detect/od/tests/test__pca/test__pca.py
+++ alibi_detect/od/tests/test__pca/test__pca.py
@@ -8,11 +8,8 @@ from alibi_detect.exceptions import NotFittedError
 from sklearn.datasets import make_moons
 
 
-def make_PCA_detector(kernel=False):
-    if kernel:
-        pca_detector = PCA(n_components=2, kernel=GaussianRBF())
-    else:
-        pca_detector = PCA(n_components=2)
+def fit_PCA_detector(detector):
+    pca_detector = detector()
     x_ref = np.random.randn(100, 3)
     pca_detector.fit(x_ref)
     pca_detector.infer_threshold(x_ref, 0.1)
@@ -48,26 +45,21 @@ def test_unfitted_PCA_single_score(detector):
         f'{pca.__class__.__name__} has not been fit!'
 
 
-def test_fitted_PCA_single_score():
-    pca_detector = PCA(n_components=2)
+@pytest.mark.parametrize('detector', [
+    lambda: PCA(n_components=2),
+    lambda: PCA(n_components=2, kernel=GaussianRBF())
+])
+def test_fitted_PCA_score(detector):
+    """Test Linear and Kernel PCA detector score method.
+
+    Test Linear and Kernel PCA detector that has been fitted on reference data but has not had a threshold
+    inferred can still score data using the predict method. Test that it does not raise an error
+    and does not return `threshold`, `p_value` and `is_outlier` values.
+    """
+    pca_detector = detector()
     x_ref = np.random.randn(100, 3)
     pca_detector.fit(x_ref)
     x = np.array([[0, 10, 0], [0.1, 0, 0]])
-    y = pca_detector.predict(x)
-    y = y['data']
-    assert y['instance_score'][0] > 5
-    assert y['instance_score'][1] < 1
-    assert not y['threshold_inferred']
-    assert y['threshold'] is None
-    assert y['is_outlier'] is None
-    assert y['p_value'] is None
-
-
-def test_fitted_kernel_PCA_single_score():
-    pca_detector = PCA(n_components=2, kernel=GaussianRBF())
-    x_ref = np.random.randn(100, 3) * np.array([1, 10, 0.1])
-    pca_detector.fit(x_ref)
-    x = np.array([[0, 5, 10], [0.1, 5, 0]])
     y = pca_detector.predict(x)
     y = y['data']
     assert y['instance_score'][0] > y['instance_score'][1]
@@ -77,27 +69,21 @@ def test_fitted_kernel_PCA_single_score():
     assert y['p_value'] is None
 
 
-def test_fitted_PCA_predict():
-    pca_detector = make_PCA_detector()
+@pytest.mark.parametrize('detector', [
+    lambda: PCA(n_components=2),
+    lambda: PCA(n_components=2, kernel=GaussianRBF())
+])
+def test_fitted_PCA_predict(detector):
+    """Test Linear and Kernel PCA detector predict method.
+
+    Test Linear and Kernel PCA detector that has been fitted on reference data and has had a threshold
+    inferred can score data using the predict method. Test that it does not raise an error and does
+    return `threshold`, `p_value` and `is_outlier` values.
+    """
+    pca_detector = fit_PCA_detector(detector)
     x_ref = np.random.randn(100, 3)
     pca_detector.infer_threshold(x_ref, 0.1)
     x = np.array([[0, 10, 0], [0.1, 0, 0]])
-    y = pca_detector.predict(x)
-    y = y['data']
-    assert y['instance_score'][0] > 5
-    assert y['instance_score'][1] < 1
-    assert y['threshold_inferred']
-    assert y['threshold'] is not None
-    assert y['p_value'].all()
-    assert (y['is_outlier'] == [True, False]).all()
-
-
-def test_fitted_kernel_PCA_predict():
-    pca_detector = PCA(n_components=2, kernel=GaussianRBF())
-    x_ref = np.random.randn(100, 3) * np.array([1, 10, 0.1])
-    pca_detector.fit(x_ref)
-    pca_detector.infer_threshold(x_ref, 0.1)
-    x = np.array([[0, 5, 10], [0.1, 5, 0]])
     y = pca_detector.predict(x)
     y = y['data']
     assert y['instance_score'][0] > y['instance_score'][1]
@@ -107,7 +93,13 @@ def test_fitted_kernel_PCA_predict():
     assert (y['is_outlier'] == [True, False]).all()
 
 
-def test_PCA_integration():
+def test_PCA_integration(tmp_path):
+    """Test Linear PCA detector on moons dataset.
+
+    Test the Linear PCA detector on a more complex 2d example. Test that the detector can be fitted
+    on reference data and infer a threshold. Test that it differentiates between inliers and outliers.
+    Test that the detector can be scripted.
+    """
     pca_detector = PCA(n_components=1)
     X_ref, _ = make_moons(1001, shuffle=True, noise=0.05, random_state=None)
     X_ref, x_inlier = X_ref[0:1000], X_ref[1000][None]
@@ -122,21 +114,24 @@ def test_PCA_integration():
     result = result['data']['is_outlier'][0]
     assert result
 
-
-def test_PCA_integration_ts():
-    pca_detector = PCA(n_components=1)
-    X_ref, _ = make_moons(1001, shuffle=True, noise=0.05, random_state=None)
-    X_ref, x_inlier = X_ref[0:1000], X_ref[1000][None]
-    pca_detector.fit(X_ref)
-    pca_detector.infer_threshold(X_ref, 0.1)
-    x_outlier = np.array([[0, -3]])
     ts_PCA = torch.jit.script(pca_detector.backend)
     x = torch.tensor([x_inlier[0], x_outlier[0]], dtype=torch.float32)
     y = ts_PCA(x)
     assert torch.all(y == torch.tensor([False, True]))
 
+    ts_PCA.save(tmp_path / 'pca.pt')
+    pca_detector = PCA(n_components=1)
+    pca_detector = torch.load(tmp_path / 'pca.pt')
+    y = ts_PCA(x)
+    assert torch.all(y == torch.tensor([False, True]))
+
 
 def test_kernel_PCA_integration():
+    """Test kernel PCA detector on moons dataset.
+
+    Test the kernel PCA detector on a more complex 2d example. Test that the detector can be fitted
+    on reference data and infer a threshold. Test that it differentiates between inliers and outliers.
+    """
     pca_detector = PCA(n_components=10, kernel=GaussianRBF())
     X_ref, _ = make_moons(1001, shuffle=True, noise=0.05, random_state=None)
     X_ref, x_inlier = X_ref[0:1000], X_ref[1000][None]
@@ -154,6 +149,7 @@ def test_kernel_PCA_integration():
 
 @pytest.mark.skip(reason='GaussianRBF kernel does not have torchscript support yet.')
 def test_kernel_PCA_integration_ts():
+    """Test the kernel PCA detector can be scripted."""
     pca_detector = PCA(n_components=10, kernel=GaussianRBF())
     X_ref, _ = make_moons(1001, shuffle=True, noise=0.05, random_state=None)
     X_ref, x_inlier = X_ref[0:1000], X_ref[1000][None]
