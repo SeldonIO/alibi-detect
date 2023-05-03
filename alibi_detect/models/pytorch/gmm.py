6dde34c9f773f21f3bdd1209cbb00c4de50987bc
Alex Athorne <alex.athorne@seldon.io>
2023-05-03 15:01:40 +0100

    Add docstrings for GMModel

--- alibi_detect/models/pytorch/gmm.py
+++ alibi_detect/models/pytorch/gmm.py
@@ -4,6 +4,15 @@ import torch
 
 class GMMModel(nn.Module):
     def __init__(self, n_components: int, dim: int) -> None:
+        """Gaussian Mixture Model (GMM).
+
+        Parameters
+        ----------
+        n_components:
+            The number of mixture components.
+        dim:
+            The dimensionality of the data.
+        """
         super().__init__()
         self.weight_logits = nn.Parameter(torch.zeros(n_components))
         self.means = nn.Parameter(torch.randn(n_components, dim))
@@ -18,6 +27,13 @@ class GMMModel(nn.Module):
         return nn.functional.softmax(self.weight_logits, dim=0)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Compute the log-likelihood of the data.
+
+        Parameters
+        ----------
+        x:
+            Data to score.
+        """
         det = torch.linalg.det(self._inv_cov)  # Note det(A^-1)=1/det(A)
         to_means = x[:, None, :] - self.means[None, :, :]
         likelihood = ((-0.5 * (
